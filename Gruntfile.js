694c8728c0ca28044ab71ab0bccba34c9727befd
Alex Skrypnyk <alex.designworks@gmail.com>
2017-07-26 15:14:38 +1000

    Added Gruntfile.

---
+++ Gruntfile.js
@@ -0,0 +1,106 @@
+/**
+ * @file
+ * Grunt tasks.
+ *
+ * Run `grunt` for to process with dev settings.
+ * Run `grunt prod` to process with prod settings.
+ * Run `grunt watch` to start watching with dev settings.
+ */
+
+/* global module */
+var themePath = 'docroot/sites/all/themes/custom/MYSITETHEME/';
+var libraryPath = 'docroot/sites/all/libraries/';
+module.exports = function (grunt) {
+  'use strict';
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass_globbing: {
+      dev: {
+        files: {
+          [themePath + 'scss/_components.scss']: themePath + 'scss/components/**/*.scss'
+        },
+        options: {
+          useSingleQuotes: true,
+          signature: '//\n// GENERATED FILE. DO NOT MODIFY DIRECTLY.\n//'
+        }
+      }
+    },
+    concat: {
+      options: {
+        separator: '\n\n'
+      },
+      dist: {
+        src: [
+          // Uncomment below to include libraries.
+          // libraryPath + 'bootstrap/assets/javascripts/bootstrap.js',
+          themePath + 'js/**/*.js',
+          '!' + themePath + 'js/MYSITETHEME.min.js'
+        ],
+        dest: themePath + 'js/MYSITETHEME.min.js'
+      }
+    },
+    uglify: {
+      prod: {
+        options: {
+          mangle: {
+            except: ['jQuery', 'Drupal']
+          },
+          compress: {
+            drop_console: true
+          }
+        },
+        files: {
+          [themePath + 'js/MYSITETHEME.min.js']: [themePath + 'js/MYSITETHEME.min.js']
+        }
+      }
+    },
+    sass: {
+      dev: {
+        files: {
+          [themePath + 'css/MYSITETHEME.min.css']: themePath + 'scss/style.scss'
+        },
+        options: {
+          sourceMap: true,
+          outputStyle: 'expanded'
+        }
+      },
+      prod: {
+        files: {
+          [themePath + 'css/MYSITETHEME.min.css']: themePath + 'scss/style.scss'
+        },
+        options: {
+          sourceMap: false,
+          outputStyle: 'compressed'
+        }
+      }
+    },
+    watch: {
+      scripts: {
+        files: [themePath + 'js/**/*.js'],
+        tasks: ['concat'],
+        options: {
+          spawn: false
+        }
+      },
+      styles: {
+        files: [themePath + 'scss/**/*.scss'],
+        tasks: ['sass_globbing', 'sass:dev'],
+        options: {
+          livereload: true,
+          spawn: false
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-sass-globbing');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-exec');
+
+  grunt.registerTask('prod', ['sass_globbing', 'concat', 'uglify:prod', 'sass:prod']);
+  // By default, run grunt with dev settings.
+  grunt.registerTask('default', ['sass_globbing', 'concat', 'sass:dev']);
+};
