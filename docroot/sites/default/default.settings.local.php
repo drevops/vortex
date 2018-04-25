85878872552f1bd0d958d2f787ebb14fd88697bd
Alex Skrypnyk <alex.designworks@gmail.com>
2018-04-25 14:56:56 +1000

    Added default local settings and services.

---
+++ docroot/sites/default/default.settings.local.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file
+ * Settings file for local environment.
+ */
+
+// Show all error messages on the site.
+$config['system.logging']['error_level'] = 'all';
+
+// Disable caching.
+$config['system.performance']['cache']['page']['max_age'] = 0;
+$settings['cache']['bins']['render'] = 'cache.backend.null';
+$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+$settings['cache']['bins']['page'] = 'cache.backend.null';
+$settings['extension_discovery_scan_tests'] = FALSE;
+
+// Disable CSS files aggregation.
+$config['system.performance']['css']['preprocess'] = FALSE;
+
+// Disable JavaScript files aggregation.
+$config['system.performance']['js']['preprocess'] = FALSE;
+
+// Skip permissions hardening.
+$settings['skip_permissions_hardening'] = TRUE;
+
+// Enable Livereload.
+$settings['livereload'] = TRUE;
