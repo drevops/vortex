38eb71861ac2ad79428f859f1de653e6aed56fdc
Alex Skrypnyk <alex.designworks@gmail.com>
2018-11-30 09:43:10 +1100

    Fixed D8 shield Acquia hook uses D7 command.

--- hooks/library/enable-shield.sh
+++ hooks/library/enable-shield.sh
@@ -7,5 +7,5 @@ site="$1"
 target_env="$2"
 
 drush @$site.$target_env en shield -y
-drush @$site.$target_env vset -y shield_user mysite
-drush @$site.$target_env vset -y shield_pass mysite
+drush @$site.$target_env -y config-set shield.settings credentials.shield.user mysite
+drush @$site.$target_env -y config-set shield.settings credentials.shield.pass mysite
