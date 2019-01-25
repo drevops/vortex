5a59ef88a68178116481da3b41b189337252f3dc
Alex Skrypnyk <alex.designworks@gmail.com>
2019-01-25 14:30:37 +1100

    Updated sanitize comments.

--- scripts/sanitize.sql
+++ scripts/sanitize.sql
@@ -1,2 +1,5 @@
 -- Example of custom sanitisation query.
+--
+-- Sanitisation runs straight after database import and before any updates
+-- and site bootstrap. It is useful to avoid working on real DB data.
 UPDATE `users_field_data` SET `status` = '0' WHERE `uid` = '1' AND `langcode` = 'en';
