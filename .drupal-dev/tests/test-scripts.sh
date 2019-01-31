c8a4879841f72a51dbbec8aab253da7faa3c392d
Alex Skrypnyk <alex.designworks@gmail.com>
2019-02-01 02:58:05 +1100

    Fixed scripts linter.

--- .drupal-dev/tests/test-scripts.sh
+++ .drupal-dev/tests/test-scripts.sh
@@ -9,9 +9,15 @@ CUR_DIR="$(cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")/.." && pwd)"
 targets=()
 while IFS=  read -r -d $'\0'; do
     targets+=("$REPLY")
-done < <(find "${CUR_DIR}"/scripts "${CUR_DIR}"/.circleci "${CUR_DIR}"/.drupal-dev -type f -name "*.sh" -print0)
+done < <(
+  find \
+    "${CUR_DIR}"/scripts \
+    "${CUR_DIR}"/.circleci \
+    "${CUR_DIR}"/.drupal-dev \
+    -type f -name "*.sh" -print0
+  )
 
 echo "==> Start linting scripts in ${CUR_DIR}"
 for file in "${targets[@]}"; do
-  LC_ALL=C.UTF-8 shellcheck "${file}";
+  [ -f "${file}" ] && LC_ALL=C.UTF-8 shellcheck "${file}";
 done;
