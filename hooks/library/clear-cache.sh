a27258dc6dca77396b5734c83cdc81aaf30a73ad
Alex Skrypnyk <alex.designworks@gmail.com>
2019-04-02 21:00:04 +1100

    Updated Acuia hooks to be more verbose.

--- hooks/library/clear-cache.sh
+++ hooks/library/clear-cache.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -e
+set -x
 #
 # Cloud Hook: cache-clear
 #
 # Run drush cache-clear all in the target environment. This script works as
 # any Cloud hook.
-set -e
 
 site="$1"
 target_env="$2"
