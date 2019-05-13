0ea9c7e029e65274dde23f941044384a7456bc5a
Alex Skrypnyk <alex.designworks@gmail.com>
2019-05-14 00:55:11 +1000

    Addedd support for unit tests.

---
+++ docroot/modules/custom/your_site_core/tests/src/Unit/YourSiteCoreHelperUnitTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\Tests\your_site_core\Unit;
+
+/**
+ * Class YourSiteCoreExampleUnitTest.
+ */
+class YourSiteCoreExampleUnitTest extends YourSiteCoreUnitTestCase {
+
+  /**
+   * @dataProvider dataProviderAdd
+   */
+  public function testAdd($a, $b, $expected, $excpectExceptionMessage = NULL) {
+    if ($excpectExceptionMessage) {
+      $this->setExpectedException(\Exception::class, $excpectExceptionMessage);
+    }
+
+    // Replace below with a call to your class method.
+    $actual = $a + $b;
+
+    if (!$excpectExceptionMessage) {
+      $this->assertEquals($expected, $actual);
+    }
+  }
+
+  public function dataProviderAdd() {
+    return [
+      [0, 0, 0],
+      [1, 1, 2],
+    ];
+  }
+
+}
