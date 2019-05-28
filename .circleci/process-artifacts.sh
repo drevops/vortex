012c3dfdc27978eaec16d4a6720e82758a07bd95
Alex Skrypnyk <alex.designworks@gmail.com>
2019-05-28 14:47:38 +1000

    Fixed screenshots creation during artifact copying in CI.

--- .circleci/process-artifacts.sh
+++ .circleci/process-artifacts.sh
@@ -4,6 +4,10 @@
 #
 set -e
 
+# Create screenshots directory in case it was not created before. This is to
+# avoid this script to fail when copying artifacts.
+ahoy cli "mkdir -p /app/screenshots"
+
 # Copy from the app container to the build host for storage.
 mkdir -p /tmp/artifacts/behat
 docker cp "$(docker-compose ps -q cli)":/app/screenshots /tmp/artifacts/behat
