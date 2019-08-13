ef5194a78e7a62b5109f169cbd927574519a1fa3
Alex Skrypnyk <alex.designworks@gmail.com>
2019-08-13 16:15:46 +1000

    Fixed xdebug for FPM. Part2.

--- .docker/Dockerfile.php
+++ .docker/Dockerfile.php
@@ -7,6 +7,5 @@ FROM amazeeio/php:7.2-fpm-v0.22.1
 # Enable Xdebug only if XDEBUG_ENABLE has a value.
 # @see https://github.com/amazeeio/lagoon/issues/1170
 COPY .docker/scripts/60-php-xdebug.sh /lagoon/entrypoints/60-php-xdebug.sh
-COPY .docker/scripts/61-php-xdebug-cli-env.sh /lagoon/entrypoints/61-php-xdebug-cli-env.sh
 
 COPY --from=cli /app /app
