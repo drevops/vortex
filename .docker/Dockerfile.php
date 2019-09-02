bfe0a7117aa533766b606ffdb531a4f6415ca46e
Alex Skrypnyk <alex.designworks@gmail.com>
2019-09-02 10:48:03 +1000

    Removed temporary overrides for XDebug.

--- .docker/Dockerfile.php
+++ .docker/Dockerfile.php
@@ -4,8 +4,4 @@ FROM ${CLI_IMAGE:-cli} as cli
 
 FROM amazeeio/php:7.2-fpm-v1.0.1
 
-# Enable Xdebug only if XDEBUG_ENABLE has a value.
-# @see https://github.com/amazeeio/lagoon/issues/1170
-COPY .docker/scripts/60-php-xdebug.sh /lagoon/entrypoints/60-php-xdebug.sh
-
 COPY --from=cli /app /app
