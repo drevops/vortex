d1cecd09c3b72db8701bfb46f9a4580c6a673a48
Alex Skrypnyk <alex@integratedexperts.com>
2019-09-03 14:07:26 +1000

    Updated Docker images to v1.0.1 and removed XDebug custom config. (#520)
    * Updated Amazeeio images to v1.0.1.
    
    * Removed temporary overrides for XDebug.
    
    * Added temp fix for NPM notworking correctly.
    
    * Upgraded node-sass.
    
    * Updated URLs for databases.
    
    * Updated apk upgrade.

--- .docker/Dockerfile.php
+++ .docker/Dockerfile.php
@@ -2,10 +2,6 @@
 ARG CLI_IMAGE
 FROM ${CLI_IMAGE:-cli} as cli
 
-FROM amazeeio/php:7.2-fpm-v0.22.1
-
-# Enable Xdebug only if XDEBUG_ENABLE has a value.
-# @see https://github.com/amazeeio/lagoon/issues/1170
-COPY .docker/scripts/60-php-xdebug.sh /lagoon/entrypoints/60-php-xdebug.sh
+FROM amazeeio/php:7.2-fpm-v1.0.1
 
 COPY --from=cli /app /app
