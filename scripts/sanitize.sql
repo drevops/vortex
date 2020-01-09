a6c68a52637d34ef59f4149df90838c1841b8a24
Alex Skrypnyk <alex@integratedexperts.com>
2020-01-10 10:49:24 +1100

    Added more DB sanitization examples.

--- scripts/sanitize.sql
+++ scripts/sanitize.sql
@@ -6,5 +6,13 @@ UPDATE `users` SET `status` = '0' WHERE `uid` = '1';
 
 -- Below are the largest tables in the database that can be truncated.
 
--- CREATE TABLE if NOT EXISTS `watchdog`;
+-- Remove webform submissions.
+-- CREATE TABLE IF NOT EXISTS `webform_submitted_data`;
+-- TRUNCATE TABLE `webform_submitted_data`;
+
+-- Remove queued items.
+-- CREATE TABLE IF NOT EXISTS `queue`;
+-- TRUNCATE TABLE `queue`;
+
+-- CREATE TABLE IF NOT EXISTS `watchdog`;
 -- TRUNCATE TABLE `watchdog`;
