cc35e80cbe63d4d8bf21b6357913368a3da4545a
Alex Skrypnyk <alex@integratedexperts.com>
2020-01-10 17:42:43 +1100

    Fixed variables format in info script.

--- scripts/drevops/info.sh
+++ scripts/drevops/info.sh
@@ -10,21 +10,21 @@ SHOW_LOGIN_LINK="${SHOW_LOGIN_LINK:-}"
 
 # ------------------------------------------------------------------------------
 
-echo  "Project                  : $PROJECT"
-echo  "Site local URL           : http://$LOCALDEV_URL"
-echo  "Path to project          : $APP"
-echo  "Path to docroot          : $APP/$WEBROOT"
-echo  "DB host                  : $AMAZEEIO_DB_HOST"
-echo  "DB username              : $AMAZEEIO_DB_USERNAME"
-echo  "DB password              : $AMAZEEIO_DB_PASSWORD"
-echo  "DB port                  : $AMAZEEIO_DB_PORT"
-echo  "DB port on host          : $HOST_DB_PORT"
+echo  "Project                  : ${PROJECT}"
+echo  "Site local URL           : http://${LOCALDEV_URL}"
+echo  "Path to project          : ${APP}"
+echo  "Path to docroot          : ${APP}/${WEBROOT}"
+echo  "DB host                  : ${AMAZEEIO_DB_HOST}"
+echo  "DB username              : ${AMAZEEIO_DB_USERNAME}"
+echo  "DB password              : ${AMAZEEIO_DB_PASSWORD}"
+echo  "DB port                  : ${AMAZEEIO_DB_PORT}"
+echo  "DB port on host          : ${HOST_DB_PORT}"
 if [ -n "${HOST_SOLR_PORT}" ]; then
-echo  "Solr port on host        : $HOST_SOLR_PORT"
+echo  "Solr port on host        : ${HOST_SOLR_PORT}"
 fi
 echo  "Mailhog URL              : http://mailhog.docker.amazee.io/"
 echo  "Xdebug                   : $(php -v | grep -q Xdebug && echo "Enabled" || echo "Disabled")"
 # For performance, generate one-time login link only if explicitly requested.
-if [ -n "$SHOW_LOGIN_LINK" ] || [ -n "${1}" ]; then
+if [ -n "${SHOW_LOGIN_LINK}" ] || [ -n "${1}" ]; then
   echo  "One-time login           : $(drush uublk 1 -q && drush uli -l "${LOCALDEV_URL}" --no-browser)"
 fi
