18a7f7c9ad13a820c6b54d1acc9b9bbdd707902e
Alex Skrypnyk <alex@integratedexperts.com>
2020-01-18 10:04:19 +1100

    Fixed code standard in Acquia hooks scripts.

--- hooks/library/notify-newrelic.sh
+++ hooks/library/notify-newrelic.sh
@@ -6,32 +6,34 @@
 set -e
 set -x
 
-site=$1
-target_env=$2
-deploy_ref=$4
-
-CONFIG_FILE="${CONFIG_FILE:-$HOME/newrelic.variables.sh}"
-NEWRELIC_ENDPOINT="${NEWRELIC_ENDPOINT:-https://api.newrelic.com/deployments.xml}"
+SITE=$1
+TARGET_ENV=$2
+DEPLOY_REF=$4
 
 NEWRELIC_USER="${NEWRELIC_USER:-}"
 NEWRELIC_APPID="${NEWRELIC_APPID:-}"
 NEWRELIC_APIKEY="${NEWRELIC_APIKEY:-}"
 
+# ------------------------------------------------------------------------------
+
+CONFIG_FILE="${CONFIG_FILE:-${HOME}/newrelic.variables.sh}"
+NEWRELIC_ENDPOINT="${NEWRELIC_ENDPOINT:-https://api.newrelic.com/deployments.xml}"
+
 if [ -f "${CONFIG_FILE}" ]; then
   # shellcheck disable=SC1090
   . "${CONFIG_FILE}"
 
-  [ -z "${NEWRELIC_USER}" ] && echo "Missing required value for NEWRELIC_USER" && exit 1
-  [ -z "${NEWRELIC_APPID}" ] && echo "Missing required value for NEWRELIC_APPID" && exit 1
-  [ -z "${NEWRELIC_APIKEY}" ] && echo "Missing required value for NEWRELIC_APIKEY" && exit 1
+  [ -z "${NEWRELIC_USER}" ] && echo "Missing required value for \$NEWRELIC_USER" && exit 1
+  [ -z "${NEWRELIC_APPID}" ] && echo "Missing required value for \$NEWRELIC_APPID" && exit 1
+  [ -z "${NEWRELIC_APIKEY}" ] && echo "Missing required value for \$NEWRELIC_APIKEY" && exit 1
 
   curl -s \
     -H "x-api-key:${NEWRELIC_APIKEY}" \
     -d "deployment[application_id]=${NEWRELIC_APPID}" \
     -d "deployment[host]=localhost" \
-    -d "deployment[description]=${deploy_ref} deployed to $site:$target_env" \
-    -d "deployment[revision]=$deploy_ref" \
-    -d "deployment[changelog]=$deploy_ref deployed to $site:$target_env" \
+    -d "deployment[description]=${DEPLOY_REF} deployed to $SITE:$TARGET_ENV" \
+    -d "deployment[revision]=$DEPLOY_REF" \
+    -d "deployment[changelog]=$DEPLOY_REF deployed to $SITE:$TARGET_ENV" \
     -d "deployment[user]=${NEWRELIC_USER}" \
     "${NEWRELIC_ENDPOINT}"
 else
