0dba6c122f072be3067a5e584a94802ae9392eed
Alex Skrypnyk <alex@integratedexperts.com>
2020-01-18 10:04:19 +1100

    Fixed code standard in Acquia hooks scripts.

--- hooks/library/install-site.sh
+++ hooks/library/install-site.sh
@@ -7,12 +7,12 @@ set -e
 set -x
 
 # Create drush alias from arguments.
-DRUSH_ALIAS="@$1.$2"
+export DRUSH_ALIAS="@${1}.${2}"
 
 # Override configuration label.
-DRUPAL_CONFIG_LABEL=vcs
+export DRUPAL_CONFIG_LABEL="vcs"
 
 # Skip DB import as it is managed through UI.
-SKIP_DB_IMPORT=1
+export SKIP_DB_IMPORT=1
 
 ./scripts/drevops/drupal-install-site.sh
