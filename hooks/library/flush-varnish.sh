bd611be1464352c85571049525b3b81157620b1b
Alex Skrypnyk <alex@integratedexperts.com>
2020-01-28 20:42:49 +1100

    Fixed varnish clear script in Acquia hooks.

--- hooks/library/flush-varnish.sh
+++ hooks/library/flush-varnish.sh
@@ -23,31 +23,31 @@ DOMAINS_FILE="${DOMAINS_FILE:-${APP}/hooks/library/domains.txt}"
 [ ! -f "${HOME}/.acquia/cloudapi.conf" ] && echo "ERROR: Acquia Cloud API credentials file ${HOME}/.acquia/cloudapi.conf does not exist." && exit 1
 
 while read -r domain; do
-  current_env="${TARGET_ENV}"
+  # Special variable to remap target env to the sub-domain prefix based on UI name.
+  TARGET_ENV_REMAP="${TARGET_ENV}"
   # Strip placeholder for PROD environment.
-  if [ "${current_env}" == "prod" ] ; then
-    domain="${domain//\$this_env./}"
+  if [ "${TARGET_ENV}" == "prod" ] ; then
+    domain="${domain//\$TARGET_ENV_REMAP./}"
+    domain="${domain//\$TARGET_ENV./}"
   fi
 
-  # Re-map 'test' to 'stg'.
-  if [ "${current_env}" == "test" ] ; then
-    current_env=stage
+  # Re-map 'test' to 'stage' as seen in UI.
+  if [ "${TARGET_ENV}" == "test" ] ; then
+    TARGET_ENV_REMAP=stage
   fi
 
   # Disable replacement for unknown environments.
-  if [ "${current_env}" != "dev" ] && [ "${current_env}" != "stage" ] ; then
-    current_env=""
+  if [ "${TARGET_ENV}" != "dev" ] && [ "${TARGET_ENV}" != "test" ] && [ "${TARGET_ENV}" != "prod" ]; then
+    TARGET_ENV_REMAP=""
   fi
 
   # Proceed only if the environment was provided.
-  if [ "${current_env}" != "" ] ; then
+  if [ "${TARGET_ENV_REMAP}" != "" ] ; then
     # Interpolate variables in domain name.
     domain="$(eval echo "${domain}")"
 
     # Clear varnish cache.
     # shellcheck disable=SC2086
-    echo drush @${SITE}.${current_env} ac-domain-purge "${domain}"
-    # shellcheck disable=SC2086
-    drush @${SITE}.${current_env} ac-domain-purge "${domain}" || true
+    echo drush @${SITE}.${TARGET_ENV} ac-domain-purge "${domain}" || true
   fi
 done < "${DOMAINS_FILE}"
