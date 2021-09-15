87f606351cc95938966952b39b9aa182ddd8df09
Alex Skrypnyk <alex@integratedexperts.com>
2021-09-16 05:57:39 +1000

    Updated test fixtures to be compatible with the latest coding standards.

--- scripts/drevops/tests/unit/InstallerDotEnvTest.php
+++ scripts/drevops/tests/unit/InstallerDotEnvTest.php
@@ -5,6 +5,8 @@ namespace Drevops\Tests;
 /**
  * Class InstallerDotEnvTest.
  *
+ * InstallerDotEnvTest fixture class.
+ *
  * phpcs:disable Drupal.Commenting.FunctionComment.Missing
  * phpcs:disable Drupal.Commenting.DocComment.MissingShort
  */
