76b9cb993e33f9733aaf5b25c41ca50ba7b5fa7a
Alex Skrypnyk <alex@integratedexperts.com>
2021-11-22 14:17:52 +1100

    Updated images versions to use M1 compatible images.

--- .docker/Dockerfile.php
+++ .docker/Dockerfile.php
@@ -5,8 +5,8 @@
 ARG CLI_IMAGE
 FROM ${CLI_IMAGE:-cli} as cli
 
-# @see https://hub.docker.com/r/amazeeio/php/tags?page=1&name=fpm
+# @see https://hub.docker.com/r/uselagoon/php/tags?page=1&name=fpm
 # @see https://github.com/uselagoon/lagoon-images/tree/main/images/php/fpm
-FROM amazeeio/php:7.4-fpm-21.7.0
+FROM uselagoon/php-7.4-fpm:21.11.0
 
 COPY --from=cli /app /app
