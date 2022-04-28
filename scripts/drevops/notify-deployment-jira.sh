bcd5402d1483839fcb995114c0b6a52cbf4fa45e
Alex Skrypnyk <alex@integratedexperts.com>
2022-04-28 23:26:31 +1000

    Added GitHub, NewRelic and JIRA notifications.

---
+++ scripts/drevops/notify-deployment-jira.sh
@@ -0,0 +1,174 @@
+#!/usr/bin/env bash
+##
+# JIRA deployment notification.
+#
+# - posts comment with a URL of a deployment environment
+# - moves an issues to a state
+# - assigns an issue to a dedicated user
+#
+
+set -e
+[ -n "${DREVOPS_DEBUG}" ] && set -x
+
+{ [ "${SKIP_NOTIFY_DEPLOYMENT}" == "1" ] || [ "${SKIP_NOTIFY_GITHUB_DEPLOYMENT}" == "1" ]; } && echo "Skipping notification of GitHub deployment." && exit 0
+
+# JIRA user.
+NOTIFY_DEPLOY_JIRA_USER="${NOTIFY_DEPLOY_JIRA_USER:-}"
+
+# JIRA token.
+NOTIFY_DEPLOY_JIRA_TOKEN="${NOTIFY_DEPLOY_JIRA_TOKEN:-}"
+
+# Deployment reference, such as a git SHA.
+NOTIFY_DEPLOY_BRANCH="${NOTIFY_DEPLOY_BRANCH:-}"
+
+# Deployment environment URL.
+NOTIFY_DEPLOY_ENVIRONMENT_URL="${NOTIFY_DEPLOY_ENVIRONMENT_URL:-}"
+
+NOTIFY_DEPLOY_JIRA_COMMENT="${NOTIFY_DEPLOY_JIRA_COMMENT:-"Deployed to "}"
+
+# State to move the ticket to.
+NOTIFY_DEPLOY_JIRA_TRANSITION="${NOTIFY_DEPLOY_JIRA_TRANSITION:-}"
+
+# Assign the ticket to this account.
+NOTIFY_DEPLOY_JIRA_ASSIGNEE="${NOTIFY_DEPLOY_JIRA_ASSIGNEE:-}"
+
+# JIRA API endpoint.l
+NOTIFY_JIRA_ENDPOINT="${NOTIFY_JIRA_ENDPOINT:-https://jira.atlassian.com}"
+
+# ------------------------------------------------------------------------------
+
+[ -z "${NOTIFY_DEPLOY_JIRA_USER}" ] && echo "ERROR: Missing required value for NOTIFY_DEPLOY_JIRA_USER" && exit 1
+[ -z "${NOTIFY_DEPLOY_JIRA_TOKEN}" ] && echo "ERROR: Missing required value for NOTIFY_DEPLOY_JIRA_TOKEN" && exit 1
+[ -z "${NOTIFY_DEPLOY_BRANCH}" ] && echo "ERROR: Missing required value for NOTIFY_DEPLOY_BRANCH" && exit 1
+[ -z "${NOTIFY_DEPLOY_JIRA_COMMENT}" ] && [ -z "${NOTIFY_DEPLOY_JIRA_TRANSITION}" ] && [ -z "${NOTIFY_DEPLOY_JIRA_ASSIGNEE}" ] && echo "ERROR: At least one of the NOTIFY_DEPLOY_JIRA_COMMENT, NOTIFY_DEPLOY_JIRA_TRANSITION or NOTIFY_DEPLOY_JIRA_ASSIGNEE is required" && exit 1
+
+echo "==> Started JIRA deployment notification"
+
+#
+# Function to extract last value from JSON object passed via STDIN.
+#
+extract_json_first_value() {
+  local key=$1
+  php -r "\$data=json_decode(file_get_contents('php://stdin'), TRUE); \$first=reset(\$data); isset(\$first[\"${key}\"]) ? print trim(json_encode(\$first[\"${key}\"], JSON_UNESCAPED_SLASHES), '\"') : exit(1);"
+}
+
+#
+# Function to extract keyed value from JSON object passed via STDIN.
+#
+extract_json_value() {
+  local key=$1
+  php -r "\$data=json_decode(file_get_contents('php://stdin'), TRUE); isset(\$data[\"${key}\"]) ? print trim(json_encode(\$data[\"${key}\"], JSON_UNESCAPED_SLASHES), '\"') : exit(1);"
+}
+
+#
+# Function to extract keyed value from JSON object passed via STDIN.
+#
+extract_json_value_by_value() {
+  local key=$1
+  local value=$2
+  local return_key=$3
+
+  php -r "\$key=\"${key}\"; \$value=\"${value}\"; \$return_key=\"${return_key}\";\$data = json_decode(file_get_contents('php://stdin'), TRUE);\$result = array_filter(\$data, function (\$object) use (\$key, \$value, \$return_key) {return !empty(\$object[\$key]) && \$object[\$key] == \$value;});\$result=reset(\$result);print trim(json_encode((\$result[\$return_key] ?? ''), JSON_UNESCAPED_SLASHES), '\"');"
+}
+
+#
+# Extract issue ID from the branch.
+#
+extract_issue() {
+  echo "$1"|sed -nE "s/([^\/]+\/)?([A-Za-z0-9]+\-[0-9]+).*/\2/p"
+}
+
+echo "  > Extracting issue"
+issue="$(extract_issue "${NOTIFY_DEPLOY_BRANCH}")"
+[ "${issue}" == "" ] && echo "ERROR: Branch ${NOTIFY_DEPLOY_BRANCH} does not contain issue number." && exit 1
+echo "    Found issue ${issue}"
+
+echo "  > Creating API token"
+token="$(echo -n "${NOTIFY_DEPLOY_JIRA_USER}:${NOTIFY_DEPLOY_JIRA_TOKEN}" | base64 -w 0)"
+
+echo -n "  > Checking API access..."
+payload="$(curl \
+ -s \
+ -X GET \
+ -H "Authorization: Basic ${token}" \
+ -H "Content-Type: application/json" \
+ "${NOTIFY_JIRA_ENDPOINT}/rest/api/3/myself")"
+
+account_id="$(echo "${payload}" | extract_json_value "accountId" || echo "error")"
+
+{ [ "${#account_id}" != "24" ] || [ "$(expr "x$account_id" : "x[0-9a-f]*$")" -eq 0 ]; } \
+&& { [ "${#account_id}" != "43" ] || [ "$(expr "x$account_id" : "x[0-9]*:[0-9a-f\-]*$")" -eq 0 ]; } \
+&& echo "ERROR: Failed to authenticate" && echo "${payload}" && exit 1
+echo "success"
+
+if [ -n "${NOTIFY_DEPLOY_JIRA_COMMENT}" ]; then
+  echo "  > Posting a comment"
+
+  [ -z "${NOTIFY_DEPLOY_ENVIRONMENT_URL}" ] && echo "ERROR: Missing required value for NOTIFY_DEPLOY_ENVIRONMENT_URL" && exit 1
+
+  comment="[{\"type\": \"text\",\"text\": \"${NOTIFY_DEPLOY_JIRA_COMMENT}\"},{\"type\": \"inlineCard\",\"attrs\": {\"url\": \"${NOTIFY_DEPLOY_ENVIRONMENT_URL}\"}}]"
+  payload="$(curl \
+    -s \
+    -X POST \
+    -H "Authorization: Basic ${token}" \
+    -H "Content-Type: application/json" \
+    --url "${NOTIFY_JIRA_ENDPOINT}/rest/api/3/issue/${issue}/comment" \
+    --data "{\"body\": {\"type\": \"doc\", \"version\": 1, \"content\": [{\"type\": \"paragraph\", \"content\": ${comment}}]}}")"
+
+  comment_id="$(echo "${payload}" | extract_json_value "id" || echo "error")"
+  [ "$(expr "x$comment_id" : "x[0-9]*$")" -eq 0 ] &&  echo "ERROR: Failed to create a comment" && exit 1
+
+  echo "    Successfully posted comment ${comment_id}"
+fi
+
+if [ -n "${NOTIFY_DEPLOY_JIRA_TRANSITION}" ]; then
+  echo "  > Changing issue status to ${NOTIFY_DEPLOY_JIRA_TRANSITION}"
+
+  echo -n "  > Discovering transition ID for ${NOTIFY_DEPLOY_JIRA_TRANSITION}..."
+  payload="$(curl \
+    -s \
+    -X GET \
+    -H "Authorization: Basic ${token}" \
+    -H "Content-Type: application/json" \
+    --url "${NOTIFY_JIRA_ENDPOINT}/rest/api/3/issue/${issue}/transitions")"
+
+  transition_id="$(echo "${payload}" | extract_json_value "transitions" | extract_json_value_by_value "name" "${NOTIFY_DEPLOY_JIRA_TRANSITION}" "id" || echo "error")"
+  { [ "${transition_id}" == "" ] || [ "$(expr "x$transition_id" : "x[0-9]*$")" -eq 0 ]; } &&  echo "ERROR: Failed to retrieve transition ID" && exit 1
+  echo -n "success"
+
+  payload="$(curl \
+    -s \
+    -X POST \
+    -H "Authorization: Basic ${token}" \
+    -H "Content-Type: application/json" \
+    --url "${NOTIFY_JIRA_ENDPOINT}/rest/api/3/issue/${issue}/transitions" \
+    --data "{ \"transition\": {\"id\": \"${transition_id}\"}}")"
+fi
+
+if [ -n "${NOTIFY_DEPLOY_JIRA_ASSIGNEE}" ]; then
+  echo "  > Assigning an issue to ${NOTIFY_DEPLOY_JIRA_ASSIGNEE}"
+
+  echo -n "  > Discovering user ID for ${NOTIFY_DEPLOY_JIRA_ASSIGNEE}..."
+  payload="$(curl \
+    -s \
+    -X GET \
+    -H "Authorization: Basic ${token}" \
+    -H "Content-Type: application/json" \
+    --url "${NOTIFY_JIRA_ENDPOINT}/rest/api/3/user/assignable/search?query=${NOTIFY_DEPLOY_JIRA_ASSIGNEE}&issueKey=${issue}")"
+
+  account_id="$(echo "${payload}" | extract_json_first_value "accountId" || echo "error")"
+  { [ "${#account_id}" != "24" ] || [ "$(expr "x$account_id" : "x[0-9a-f]*$")" -eq 0 ]; } \
+    && { [ "${#account_id}" != "43" ] || [ "$(expr "x$account_id" : "x[0-9]*:[0-9a-f\-]*$")" -eq 0 ]; } \
+    && echo "ERROR: Failed to retrieve assignee account ID" && echo "${payload}" && exit 1
+  echo -n "success"
+
+  payload="$(curl \
+    -s \
+    -X PUT \
+    -H "Authorization: Basic ${token}" \
+    -H "Content-Type: application/json" \
+    --url "${NOTIFY_JIRA_ENDPOINT}/rest/api/3/issue/${issue}/assignee" \
+    --data "{ \"accountId\": \"${account_id}\"}")"
+fi
+
+echo "==> Finished JIRA deployment notification"
---
+++ scripts/drevops/notify-deployment-newrelic.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+##
+# New Relic deployment notification.
+#
+
+set -e
+[ -n "${DREVOPS_DEBUG}" ] && set -x
+
+[ "${SKIP_NOTIFY_DEPLOYMENT}" == "1" ] && echo "Skipping notify deployment." && exit 0
+
+# The API key. Usually of type 'USER'.
+NOTIFY_NEWRELIC_APIKEY="${NOTIFY_NEWRELIC_APIKEY:-}"
+
+# Deployment reference, such as a git SHA.
+NOTIFY_DEPLOY_REF="${NOTIFY_DEPLOY_REF:-}"
+
+# Application name as it appears in the dashboard.
+NOTIFY_NEWRELIC_APPNAME="${NOTIFY_NEWRELIC_APPNAME:-}"
+
+# Optional Application ID. Will be discovered automatically from application name if not provided.
+NOTIFY_NEWRELIC_APPID="${NOTIFY_NEWRELIC_APPID:-}"
+
+# Optional deployment description.
+NOTIFY_NEWRELIC_DESCRIPTION="${NOTIFY_NEWRELIC_DESCRIPTION:-"${NOTIFY_DEPLOY_REF} deployed"}"
+
+# Optional deployment changelog. Defaults to description.
+NOTIFY_NEWRELIC_CHANGELOG="${NOTIFY_NEWRELIC_CHANGELOG:-${NOTIFY_NEWRELIC_DESCRIPTION}}"
+
+# Optional user name performing the deployment.
+NOTIFY_NEWRELIC_USER="${NOTIFY_NEWRELIC_USER:-"Deployment robot"}"
+
+# Optional endpoint.
+NOTIFY_NEWRELIC_ENDPOINT="${NOTIFY_NEWRELIC_ENDPOINT:-https://api.newrelic.com/v2}"
+
+# ------------------------------------------------------------------------------
+
+[ -z "${NOTIFY_NEWRELIC_APIKEY}" ] && echo "ERROR: Missing required value for NOTIFY_NEWRELIC_APIKEY" && exit 1
+[ -z "${NOTIFY_DEPLOY_REF}" ] && echo "ERROR: Missing required value for NOTIFY_DEPLOY_REF" && exit 1
+[ -z "${NOTIFY_NEWRELIC_APPNAME}" ] && echo "ERROR: Missing required value for NOTIFY_NEWRELIC_APPNAME" && exit 1
+[ -z "${NOTIFY_NEWRELIC_DESCRIPTION}" ] && echo "ERROR: Missing required value for NOTIFY_NEWRELIC_DESCRIPTION" && exit 1
+[ -z "${NOTIFY_NEWRELIC_CHANGELOG}" ] && echo "ERROR: Missing required value for NOTIFY_NEWRELIC_CHANGELOG" && exit 1
+[ -z "${NOTIFY_NEWRELIC_USER}" ] && echo "ERROR: Missing required value for NOTIFY_NEWRELIC_USER" && exit 1
+
+echo "==> Started New Relic notification"
+
+# Discover APP id by name if it was not provided.
+if [ -z "${NOTIFY_NEWRELIC_APPID}" ] && [ -n "${NOTIFY_NEWRELIC_APPNAME}" ]; then
+  NOTIFY_NEWRELIC_APPID="$(curl -s -X GET "${NOTIFY_NEWRELIC_ENDPOINT}/applications.json" \
+    -H "Api-Key:${NOTIFY_NEWRELIC_APIKEY}" \
+    -s -G -d "filter[name]=${NOTIFY_NEWRELIC_APPNAME}&exclude_links=true" |
+    cut -c 24- |
+    cut -c -10)"
+fi
+
+{ [ "${#NOTIFY_NEWRELIC_APPID}" != "10" ] || [ "$(expr "x$NOTIFY_NEWRELIC_APPID" : "x[0-9]*$")" -eq 0 ]; } && echo "ERROR: Failed to get an application ID from the application name ${NOTIFY_NEWRELIC_APPNAME}." && exit 1
+
+if ! curl -X POST "${NOTIFY_NEWRELIC_ENDPOINT}/applications/${NOTIFY_NEWRELIC_APPID}/deployments.json" \
+  -L -s -o /dev/null -w "%{http_code}" \
+  -H "Api-Key:${NOTIFY_NEWRELIC_APIKEY}" \
+  -H 'Content-Type: application/json' \
+  -d \
+  "{
+  \"deployment\": {
+    \"revision\": \"${NOTIFY_DEPLOY_REF}\",
+    \"changelog\": \"${NOTIFY_NEWRELIC_CHANGELOG}\",
+    \"description\": \"${NOTIFY_NEWRELIC_DESCRIPTION}\",
+    \"user\": \"${NOTIFY_NEWRELIC_USER}\"
+  }
+}" | grep -q '201'; then
+  error "ERROR: Failed to crate a deployment notification for application ${NOTIFY_NEWRELIC_APPNAME} with ID ${NOTIFY_NEWRELIC_APPID}"
+  exit 1
+fi
+
+echo "==> Finished New Relic notification"
