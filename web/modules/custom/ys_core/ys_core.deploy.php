afa95210e2dcb3a4b97634320f27fdc704bef822
Alex Skrypnyk <alex@drevops.com>
2023-04-12 11:03:10 +1000

    t3

--- web/modules/custom/ys_core/ys_core.deploy.php
+++ web/modules/custom/ys_core/ys_core.deploy.php
@@ -17,6 +17,7 @@ function ys_core_deploy_install_theme() {
   \Drupal::service('theme_installer')->install(['your_site_theme']);
   \Drupal::service('config.factory')->getEditable('system.theme')->set('default', 'your_site_theme')->save();
 }
+
 // phpcs:ignore #;< REDIS
 /**
  * Enables Redis module.
