13ac003baabcb8f3dc6ee8aa3bcf496b092e13f6
Alex Skrypnyk <alex@drevops.com>
2023-04-25 20:47:44 +1000

    Revert "test install hook to revert"
    This reverts commit 9bc23bf90787794ed2026d67cc4b67d2d159ec9e.

--- web/modules/custom/ys_core/ys_core.deploy.php
+++ web/modules/custom/ys_core/ys_core.deploy.php
@@ -15,7 +15,7 @@ use Drupal\Core\Extension\ExtensionDiscovery;
 /**
  * Installs custom theme.
  */
-function ys_core_deploy_install_theme2() {
+function ys_core_deploy_install_theme() {
   \Drupal::service('theme_installer')->install(['olivero']);
   \Drupal::service('theme_installer')->install(['your_site_theme']);
   \Drupal::service('config.factory')->getEditable('system.theme')->set('default', 'your_site_theme')->save();
