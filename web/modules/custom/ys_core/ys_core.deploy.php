9bc23bf90787794ed2026d67cc4b67d2d159ec9e
Alex Skrypnyk <alex@drevops.com>
2023-04-25 20:35:20 +1000

    test install hook to revert

--- web/modules/custom/ys_core/ys_core.deploy.php
+++ web/modules/custom/ys_core/ys_core.deploy.php
@@ -15,7 +15,7 @@ use Drupal\Core\Extension\ExtensionDiscovery;
 /**
  * Installs custom theme.
  */
-function ys_core_deploy_install_theme() {
+function ys_core_deploy_install_theme2() {
   \Drupal::service('theme_installer')->install(['olivero']);
   \Drupal::service('theme_installer')->install(['your_site_theme']);
   \Drupal::service('config.factory')->getEditable('system.theme')->set('default', 'your_site_theme')->save();
