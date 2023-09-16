11ebb6410b5a590b0acf21fe725de65a562f7e6f
Alex Skrypnyk <alex@drevops.com>
2023-09-16 15:43:14 +1000

    only coverage when the dir is set.

--- scripts/drevops/tests/test.common.sh
+++ scripts/drevops/tests/test.common.sh
@@ -10,7 +10,7 @@ SCRIPTS_DIR="scripts/drevops"
 
 TEST_DIR="${SCRIPTS_DIR}/tests"
 
-DREVOPS_TEST_COVERAGE_DIR="${DREVOPS_TEST_COVERAGE_DIR:-${TEST_DIR}/.coverage}"
+DREVOPS_TEST_COVERAGE_DIR="${DREVOPS_TEST_COVERAGE_DIR:-}"
 
 # ------------------------------------------------------------------------------
 #
@@ -51,8 +51,12 @@ echo "==> Run common functional tests."
 [ ! -d "${TEST_DIR}/node_modules" ] && echo "  > Install test Node dependencies." && npm --prefix="${TEST_DIR}" ci
 
 bats() {
-  mkdir -p ${DREVOPS_TEST_COVERAGE_DIR}
-  kcov --include-path="${SCRIPTS_DIR}" --bash-parse-files-in-dir="${SCRIPTS_DIR}" --exclude-path=${TEST_DIR}/node_modules,${TEST_DIR}/vendor,${SCRIPTS_DIR}/installer/scripts/drevops/docs "${DREVOPS_TEST_COVERAGE_DIR}" "${TEST_DIR}/node_modules/.bin/bats" "$@"
+  if [ -n "${DREVOPS_TEST_COVERAGE_DIR}" ]; then
+    mkdir -p ${DREVOPS_TEST_COVERAGE_DIR}
+    kcov --include-path="${SCRIPTS_DIR}" --bash-parse-files-in-dir="${SCRIPTS_DIR}" --exclude-path=${TEST_DIR}/node_modules,${TEST_DIR}/vendor,${SCRIPTS_DIR}/installer/scripts/drevops/docs "${DREVOPS_TEST_COVERAGE_DIR}" "${TEST_DIR}/node_modules/.bin/bats" "$@"
+  else
+    "${TEST_DIR}/node_modules/.bin/bats" "$@"
+  fi
 }
 
 bats "${TEST_DIR}/bats/helpers.bats"
