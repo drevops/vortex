4369e4b6482f01fcb99497fb00b6920f1544e3b2
Alex Skrypnyk <alex@drevops.com>
2023-09-16 17:22:28 +1000

    Added test coverage for scripts. (#995)

--- scripts/drevops/tests/test.common.sh
+++ scripts/drevops/tests/test.common.sh
@@ -6,7 +6,9 @@
 set -eu
 [ "${DREVOPS_DEBUG-}" = "1" ] && set -x
 
-TEST_DIR="scripts/drevops/tests"
+SCRIPTS_DIR="scripts/drevops"
+
+TEST_DIR="${SCRIPTS_DIR}/tests"
 
 # ------------------------------------------------------------------------------
 
@@ -47,7 +49,12 @@ echo "==> Run common functional tests."
 [ ! -d "${TEST_DIR}/node_modules" ] && echo "  > Install test Node dependencies." && npm --prefix="${TEST_DIR}" ci
 
 bats() {
-  "${TEST_DIR}/node_modules/.bin/bats" "$@"
+  if [ -n "${DREVOPS_TEST_COVERAGE_DIR:-}" ]; then
+    mkdir -p "${DREVOPS_TEST_COVERAGE_DIR}"
+    kcov --include-path="${SCRIPTS_DIR}" --bash-parse-files-in-dir="${SCRIPTS_DIR}" --exclude-path=${TEST_DIR}/node_modules,${TEST_DIR}/vendor,${SCRIPTS_DIR}/installer/scripts/drevops/docs "${DREVOPS_TEST_COVERAGE_DIR}" "${TEST_DIR}/node_modules/.bin/bats" "$@"
+  else
+    "${TEST_DIR}/node_modules/.bin/bats" "$@"
+  fi
 }
 
 bats "${TEST_DIR}/bats/helpers.bats"
