e4e7f298f4ae24038d5d94f30249226b05ec35ae
Alex Skrypnyk <alex@drevops.com>
2023-10-09 13:31:54 +1100

    Added docs about PHPCS.

--- web/themes/custom/your_site_theme/Gruntfile.js
+++ web/themes/custom/your_site_theme/Gruntfile.js
@@ -5,7 +5,7 @@
  * Run `grunt` for to process with dev settings.
  * Run `grunt prod` to process with prod settings.
  * Run `grunt watch` to start watching with dev settings.
- * @phpcs:ignoreFile
+ * phpcs:ignoreFile
  */
 
 /* global module */
