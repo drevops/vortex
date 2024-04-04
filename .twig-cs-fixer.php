36cabe1739b99b6fda33f6a3269f1aa1fb703e1f
tan.nguyen <raucaixanh04@gmail.com>
2024-04-04 09:59:36 +0700

    Replace web by [your-webroot].

--- .twig-cs-fixer.php
+++ .twig-cs-fixer.php
@@ -6,8 +6,8 @@ $ruleset = new TwigCsFixer\Ruleset\Ruleset();
 $ruleset->addStandard(new TwigCsFixer\Standard\Twig());
 
 $finder = new TwigCsFixer\File\Finder();
-$finder->in(__DIR__ . '/web/modules/custom');
-$finder->in(__DIR__ . '/web/themes/custom');
+$finder->in(__DIR__ . '/[your-webroot]/modules/custom');
+$finder->in(__DIR__ . '/[your-webroot]/themes/custom');
 
 $config = new TwigCsFixer\Config\Config();
 $config->setRuleset($ruleset);
