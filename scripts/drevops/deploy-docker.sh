2bce67974ee3431fdf4e4372d1a207cc52571d3c
tan.nguyen <raucaixanh04@gmail.com>
2024-04-17 15:12:29 +0700

    No need check some defaut commands.

--- scripts/drevops/deploy-docker.sh
+++ scripts/drevops/deploy-docker.sh
@@ -39,7 +39,7 @@ pass() { [ "${TERM:-}" != "dumb" ] && tput colors >/dev/null 2>&1 && printf "\03
 fail() { [ "${TERM:-}" != "dumb" ] && tput colors >/dev/null 2>&1 && printf "\033[31m[FAIL] %s\033[0m\n" "${1}" || printf "[FAIL] %s\n" "${1}"; }
 # @formatter:on
 
-command -v docker >/dev/null || (fail "docker command is not available." && exit 1)
+command -v docker >/dev/null || (fail "Command docker is not available." && exit 1)
 
 info "Started DOCKER deployment."
 
