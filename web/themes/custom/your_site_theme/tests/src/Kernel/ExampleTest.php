83f48bdbd68513a4b441220bc304c2112a6263ff
Alex Skrypnyk <alex@drevops.com>
2024-06-18 17:28:19 +1000

    Updated coding standards in tests.

--- web/themes/custom/your_site_theme/tests/src/Kernel/ExampleTest.php
+++ web/themes/custom/your_site_theme/tests/src/Kernel/ExampleTest.php
@@ -36,7 +36,7 @@ class ExampleTest extends YourSiteThemeKernelTestBase {
   /**
    * Data provider for testAdd().
    */
-  public function dataProviderAdd(): array {
+  public static function dataProviderAdd(): array {
     return [
       [0, 0, 0],
       [1, 1, 2],
@@ -65,7 +65,7 @@ class ExampleTest extends YourSiteThemeKernelTestBase {
   /**
    * Data provider for testSubtract().
    */
-  public function dataProviderSubtract(): array {
+  public static function dataProviderSubtract(): array {
     return [
       [0, 0, 0],
       [1, 1, 0],
@@ -96,7 +96,7 @@ class ExampleTest extends YourSiteThemeKernelTestBase {
   /**
    * Data provider for testMultiplication().
    */
-  public function dataProviderMultiplication(): array {
+  public static function dataProviderMultiplication(): array {
     return [
       [0, 0, 0],
       [1, 1, 1],
