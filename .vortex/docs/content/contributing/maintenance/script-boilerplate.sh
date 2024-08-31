drevops/vortex-destination-gha

Renamed project to `Vortex`.

.vortex/docs/content/contributing/maintenance/script-boilerplate.sh
@@ -1,0 +1,32 @@
+#!/usr/bin/env bash
+##
+# Action description that the script performs.
+#
+# More description and usage information with a last empty
+# comment line.
+#
+
+set -eu
+[ "${VORTEX_DEBUG-}" = "1" ] && set -x
+
+# Example scaffold variable with a default value.
+VORTEX_SCAFFOLD_EXAMPLE_URL="${VORTEX_SCAFFOLD_EXAMPLE_URL:-http://example.com}"
+
+# ------------------------------------------------------------------------------
+
+# @formatter:off
+note() { printf "       %s\n" "${1}"; }
+info() { [ "${TERM:-}" != "dumb" ] && tput colors >/dev/null 2>&1 && printf "\033[34m[INFO] %s\033[0m\n" "${1}" || printf "[INFO] %s\n" "${1}"; }
+pass() { [ "${TERM:-}" != "dumb" ] && tput colors >/dev/null 2>&1 && printf "\033[32m[ OK ] %s\033[0m\n" "${1}" || printf "[ OK ] %s\n" "${1}"; }
+fail() { [ "${TERM:-}" != "dumb" ] && tput colors >/dev/null 2>&1 && printf "\033[31m[FAIL] %s\033[0m\n" "${1}" || printf "[FAIL] %s\n" "${1}"; }
+# @formatter:on
+
+info "Started scaffold operations."
+
+[ -z "${VORTEX_SCAFFOLD_EXAMPLE_URL}" ] && fail "Missing required value for VORTEX_SCAFFOLD_EXAMPLE_URL" && exit 1
+command -v curl >/dev/null || (fail "curl command is not available." && exit 1)
+
+# Example of the script body.
+curl -L -s -o /dev/null -w "%{http_code}" "${VORTEX_SCAFFOLD_EXAMPLE_URL}" | grep -q '200\|403' && note "Requested example page"
+
+pass "Finished scaffold operations."

.vortex/tests/bats/install.integrations.bats
@@ -1,0 +1,206 @@
+#!/usr/bin/env bats
+#
+# Integration tests assert that all required files are present for selected
+# integrations.
+#
+# shellcheck disable=SC2030,SC2031,SC2129
+
+load _helper.bash
+
+@test "Install: empty directory; none of Deployment, Acquia, Lagoon, FTP and renovatebot integrations" {
+  answers=(
+    "Star wars" # name
+    "nothing"   # machine_name
+    "nothing"   # org
+    "nothing"   # org_machine_name
+    "nothing"   # module_prefix
+    "nothing"   # profile
+    "nothing"   # theme
+    "nothing"   # URL
+    "nothing"   # webroot
+    "nothing"   # provision_use_profile
+    "nothing"   # database_download_source
+    "nothing"   # database_store_type
+    "nothing"   # override_existing_db
+    "none"      # deploy_type
+    "no"        # preserve_ftp
+    "no"        # preserve_acquia
+    "no"        # preserve_lagoon
+    "no"        # preserve_renovatebot
+    "nothing"   # preserve_doc_comments
+    "nothing"   # preserve_drevops_info
+  )
+  output=$(run_installer_interactive "${answers[@]}")
+  assert_output_contains "WELCOME TO DREVOPS INTERACTIVE INSTALLER"
+  assert_git_repo
+
+  install_dependencies_stub
+
+  assert_files_present_common
+  assert_files_present_no_provision_use_profile
+  assert_files_present_no_deployment
+  assert_files_present_no_integration_acquia
+  assert_files_present_no_integration_lagoon
+  assert_files_present_no_integration_ftp
+  assert_files_present_no_integration_renovatebot
+}
+
+@test "Install: empty directory; all integrations" {
+  answers=(
+    "Star wars" # name
+    "nothing"   # machine_name
+    "nothing"   # org
+    "nothing"   # org_machine_name
+    "nothing"   # module_prefix
+    "nothing"   # profile
+    "nothing"   # theme
+    "nothing"   # URL
+    "nothing"   # webroot
+    "nothing"   # provision_use_profile
+    "curl"      # database_download_source
+    "file"      # database_store_type
+    "nothing"   # override_existing_db
+    "nothing"   # deploy_type
+    "y"         # preserve_ftp
+    "y"         # preserve_acquia
+    "y"         # preserve_lagoon
+    "y"         # preserve_renovatebot
+    "nothing"   # preserve_doc_comments
+    "nothing"   # preserve_drevops_info
+  )
+  output=$(run_installer_interactive "${answers[@]}")
+  assert_output_contains "WELCOME TO DREVOPS INTERACTIVE INSTALLER"
+  assert_git_repo
+
+  install_dependencies_stub
+
+  assert_files_present_common
+  assert_files_present_no_provision_use_profile
+  assert_files_present_deployment
+  assert_files_present_integration_acquia
+  assert_files_present_integration_lagoon
+  assert_files_present_integration_ftp
+  assert_files_present_integration_renovatebot
+
+  assert_file_contains ".lagoon.yml" "name: Download database"
+  assert_file_contains ".lagoon.yml" "export VORTEX_PROVISION_OVERRIDE_DB=0"
+  assert_file_not_contains ".lagoon.yml" "# Explicitly set DB overwrite flag to the value from .env file for deployments from the profile."
+}
+
+@test "Install: empty directory; deployment - code" {
+  answers=(
+    "Star wars" # name
+    "nothing"   # machine_name
+    "nothing"   # org
+    "nothing"   # org_machine_name
+    "nothing"   # module_prefix
+    "nothing"   # profile
+    "nothing"   # theme
+    "nothing"   # URL
+    "nothing"   # webroot
+    "nothing"   # provision_use_profile
+    "curl"      # database_download_source
+    "file"      # database_store_type
+    "nothing"   # override_existing_db
+    "artifact"  # deploy_type
+    "y"         # preserve_ftp
+    "y"         # preserve_acquia
+    "y"         # preserve_lagoon
+    "y"         # preserve_renovatebot
+    "nothing"   # preserve_doc_comments
+    "nothing"   # preserve_drevops_info
+  )
+  output=$(run_installer_interactive "${answers[@]}")
+  assert_output_contains "WELCOME TO DREVOPS INTERACTIVE INSTALLER"
+  assert_git_repo
+
+  install_dependencies_stub
+
+  assert_files_present_common
+  assert_files_present_no_provision_use_profile
+  assert_files_present_deployment
+  assert_files_present_integration_acquia
+  assert_files_present_integration_lagoon
+  assert_files_present_integration_ftp
+  assert_files_present_integration_renovatebot
+
+  assert_file_contains ".lagoon.yml" "name: Download database"
+  assert_file_contains ".lagoon.yml" "export VORTEX_PROVISION_OVERRIDE_DB=0"
+  assert_file_not_contains ".lagoon.yml" "# Explicitly set DB overwrite flag to the value from .env file for deployments from the profile."
+}
+
+@test "Install: empty directory; provision_use_profile" {
+  answers=(
+    "Star wars" # name
+    "nothing"   # machine_name
+    "nothing"   # org
+    "nothing"   # org_machine_name
+    "nothing"   # module_prefix
+    "nothing"   # profile
+    "nothing"   # theme
+    "nothing"   # URL
+    "nothing"   # webroot
+    "y"         # provision_use_profile
+    "nothing"   # override_existing_db
+    "artifact"  # deploy_type
+    "n"         # preserve_ftp
+    "n"         # preserve_acquia
+    "n"         # preserve_lagoon
+    "n"         # preserve_renovatebot
+    "nothing"   # preserve_doc_comments
+    "nothing"   # preserve_drevops_info
+  )
+  output=$(run_installer_interactive "${answers[@]}")
+  assert_output_contains "WELCOME TO DREVOPS INTERACTIVE INSTALLER"
+  assert_git_repo
+
+  install_dependencies_stub
+
+  assert_files_present_common
+  assert_files_present_provision_use_profile
+  assert_files_present_deployment
+  assert_files_present_no_integration_acquia
+  assert_files_present_no_integration_lagoon
+  assert_files_present_no_integration_ftp
+  assert_files_present_no_integration_renovatebot
+}
+
+@test "Install: empty directory; provision_use_profile; Lagoon" {
+  answers=(
+    "Star wars" # name
+    "nothing"   # machine_name
+    "nothing"   # org
+    "nothing"   # org_machine_name
+    "nothing"   # module_prefix
+    "nothing"   # profile
+    "nothing"   # theme
+    "nothing"   # URL
+    "nothing"   # webroot
+    "y"         # provision_use_profile
+    "nothing"   # override_existing_db
+    "artifact"  # deploy_type
+    "n"         # preserve_ftp
+    "n"         # preserve_acquia
+    "y"         # preserve_lagoon
+    "n"         # preserve_renovatebot
+    "nothing"   # preserve_doc_comments
+    "nothing"   # preserve_drevops_info
+  )
+  output=$(run_installer_interactive "${answers[@]}")
+  assert_output_contains "WELCOME TO DREVOPS INTERACTIVE INSTALLER"
+  assert_git_repo
+
+  install_dependencies_stub
+
+  assert_files_present_common
+  assert_files_present_provision_use_profile
+  assert_files_present_deployment
+  assert_files_present_no_integration_acquia
+  assert_files_present_integration_lagoon
+  assert_files_present_no_integration_ftp
+  assert_files_present_no_integration_renovatebot
+
+  assert_file_not_contains ".lagoon.yml" "name: Download database"
+  assert_file_not_contains ".lagoon.yml" "export VORTEX_PROVISION_OVERRIDE_DB=0"
+  assert_file_contains ".lagoon.yml" "# Explicitly set DB overwrite flag to the value from .env file for deployments from the profile."
+}

.vortex/tests/bats/workflow.smoke.bats
@@ -1,0 +1,86 @@
+#!/usr/bin/env bats
+#
+# Workflows using different types of install source.
+#
+
+# shellcheck disable=SC2030,SC2031,SC2129
+
+load _helper.bash
+load _helper.workflow.bash
+
+@test "Idempotence" {
+  prepare_sut "Starting idempotence tests in build directory ${BUILD_DIR}"
+
+  step "Download DEMO database"
+  assert_ahoy_download_db
+
+  step "Build project"
+  assert_ahoy_build
+  assert_gitignore
+  assert_ahoy_test_bdd_fast
+
+  # Running build several times should result in the same project build results.
+  step "Re-build project"
+  assert_ahoy_build
+  # Skip committing of the files.
+  assert_gitignore 1
+  assert_ahoy_test_bdd_fast
+}
+
+# Make sure to run with `TEST_GITHUB_TOKEN=working_test_token bats...` or this test will fail.
+@test "GitHub token" {
+  prepare_sut "Starting GitHub token tests in build directory ${BUILD_DIR}"
+
+  step "Add private package"
+  rm composer.lock || true
+  composer config repositories.test-private-package vcs git@github.com:drevops/test-private-package.git
+  jq --indent 4 '.require += {"drevops/test-private-package": "^1"}' composer.json >composer.json.tmp && mv -f composer.json.tmp composer.json
+
+  export VORTEX_CONTAINER_REGISTRY_USER="${TEST_VORTEX_CONTAINER_REGISTRY_USER?Test Docker user is not set}"
+  export VORTEX_CONTAINER_REGISTRY_PASS="${TEST_VORTEX_CONTAINER_REGISTRY_PASS?Test Docker pass is not set}"
+
+  step "Build without a GITHUB_TOKEN token"
+  unset GITHUB_TOKEN
+  process_ahoyyml
+  run ahoy build
+  assert_failure
+
+  step "Build with a GITHUB_TOKEN token"
+  export GITHUB_TOKEN="${TEST_GITHUB_TOKEN}"
+  process_ahoyyml
+  run ahoy build
+  assert_success
+}
+
+@test "Docker compose, no Ahoy" {
+  prepare_sut "Starting Docker compose without Ahoy tests in build directory ${BUILD_DIR}"
+
+  step "Download DEMO database"
+  assert_ahoy_download_db
+
+  step "Build project"
+  ahoy reset
+
+  substep "Building stack"
+  docker compose up -d --build --force-recreate >&3
+
+  substep "Installing dependencies"
+  docker compose exec -T cli composer install --prefer-dist >&3
+
+  substep "Provisioning"
+
+  # Copy DB into container for the cases when the volumes are not mounted.
+  # This will not be a case locally.
+  if [ "${SCAFFOLD_DEV_VOLUMES_MOUNTED}" != "1" ]; then
+    if [ -f .data/db.sql ]; then
+      docker compose exec cli mkdir -p .data
+      docker compose cp -L .data/db.sql cli:/app/.data/db.sql
+    fi
+  fi
+
+  docker compose exec -T cli ./scripts/vortex/provision.sh >&3
+
+  sync_to_host
+  assert_gitignore
+  assert_ahoy_test_bdd_fast
+}

.vortex/tests/test.deployment.sh
@@ -1,0 +1,41 @@
+#!/usr/bin/env bash
+##
+# Run Vortex deployment tests.
+#
+# LCOV_EXCL_START
+
+set -eu
+[ "${VORTEX_DEBUG-}" = "1" ] && set -x
+
+ROOT_DIR="$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)")"
+
+SCRIPTS_DIR="${ROOT_DIR}/scripts/vortex"
+
+TEST_DIR="${ROOT_DIR}/.vortex/tests"
+
+# ------------------------------------------------------------------------------
+
+# Configure git username and email if it is not set.
+[ "$(git config --global user.name)" = "" ] && echo "==> Configuring global git user name" && git config --global user.name "Test user"
+[ "$(git config --global user.email)" = "" ] && echo "==> Configuring global git user email" && git config --global user.email "someone@example.com"
+
+# Create stub of local framework.
+docker network create amazeeio-network 2>/dev/null || true
+
+index="${TEST_NODE_INDEX:-*}"
+echo "==> Run deployment functional tests (${index})."
+[ ! -d "${TEST_DIR}/node_modules" ] && echo "  > Install test Node dependencies." && npm --prefix="${TEST_DIR}" ci
+
+bats() {
+  pushd "${ROOT_DIR}" >/dev/null || exit 1
+  if [ -n "${SCAFFOLD_DEV_TEST_COVERAGE_DIR:-}" ]; then
+    mkdir -p "${SCAFFOLD_DEV_TEST_COVERAGE_DIR}"
+    kcov --include-pattern=.sh,.bash --bash-parse-files-in-dir="${SCRIPTS_DIR}","${TEST_DIR}" --exclude-pattern=vendor,node_modules "${SCAFFOLD_DEV_TEST_COVERAGE_DIR}" "${TEST_DIR}/node_modules/.bin/bats" "$@"
+  else
+    "${TEST_DIR}/node_modules/.bin/bats" "$@"
+  fi
+  popd >/dev/null || exit 1
+}
+
+# shellcheck disable=SC2086
+bats "${TEST_DIR}"/bats/deployment${index}.bats

drevops/vortex

.vortex/docs/content/contributing/maintenance/script-boilerplate.sh
@@ -1,0 +1,32 @@
+#!/usr/bin/env bash
+##
+# Action description that the script performs.
+#
+# More description and usage information with a last empty
+# comment line.
+#
+
+set -eu
+[ "${VORTEX_DEBUG-}" = "1" ] && set -x
+
+# Example scaffold variable with a default value.
+VORTEX_EXAMPLE_URL="${VORTEX_EXAMPLE_URL:-http://example.com}"
+
+# ------------------------------------------------------------------------------
+
+# @formatter:off
+note() { printf "       %s\n" "${1}"; }
+info() { [ "${TERM:-}" != "dumb" ] && tput colors >/dev/null 2>&1 && printf "\033[34m[INFO] %s\033[0m\n" "${1}" || printf "[INFO] %s\n" "${1}"; }
+pass() { [ "${TERM:-}" != "dumb" ] && tput colors >/dev/null 2>&1 && printf "\033[32m[ OK ] %s\033[0m\n" "${1}" || printf "[ OK ] %s\n" "${1}"; }
+fail() { [ "${TERM:-}" != "dumb" ] && tput colors >/dev/null 2>&1 && printf "\033[31m[FAIL] %s\033[0m\n" "${1}" || printf "[FAIL] %s\n" "${1}"; }
+# @formatter:on
+
+info "Started scaffold operations."
+
+[ -z "${VORTEX_EXAMPLE_URL}" ] && fail "Missing required value for VORTEX_EXAMPLE_URL" && exit 1
+command -v curl >/dev/null || (fail "curl command is not available." && exit 1)
+
+# Example of the script body.
+curl -L -s -o /dev/null -w "%{http_code}" "${VORTEX_EXAMPLE_URL}" | grep -q '200\|403' && note "Requested example page"
+
+pass "Finished scaffold operations."

.vortex/tests/bats/install.integrations.bats
@@ -1,0 +1,206 @@
+#!/usr/bin/env bats
+#
+# Integration tests assert that all required files are present for selected
+# integrations.
+#
+# shellcheck disable=SC2030,SC2031,SC2129
+
+load _helper.bash
+
+@test "Install: empty directory; none of Deployment, Acquia, Lagoon, FTP and renovatebot integrations" {
+  answers=(
+    "Star wars" # name
+    "nothing"   # machine_name
+    "nothing"   # org
+    "nothing"   # org_machine_name
+    "nothing"   # module_prefix
+    "nothing"   # profile
+    "nothing"   # theme
+    "nothing"   # URL
+    "nothing"   # webroot
+    "nothing"   # provision_use_profile
+    "nothing"   # database_download_source
+    "nothing"   # database_store_type
+    "nothing"   # override_existing_db
+    "none"      # deploy_type
+    "no"        # preserve_ftp
+    "no"        # preserve_acquia
+    "no"        # preserve_lagoon
+    "no"        # preserve_renovatebot
+    "nothing"   # preserve_doc_comments
+    "nothing"   # preserve_vortex_info
+  )
+  output=$(run_installer_interactive "${answers[@]}")
+  assert_output_contains "WELCOME TO VORTEX INTERACTIVE INSTALLER"
+  assert_git_repo
+
+  install_dependencies_stub
+
+  assert_files_present_common
+  assert_files_present_no_provision_use_profile
+  assert_files_present_no_deployment
+  assert_files_present_no_integration_acquia
+  assert_files_present_no_integration_lagoon
+  assert_files_present_no_integration_ftp
+  assert_files_present_no_integration_renovatebot
+}
+
+@test "Install: empty directory; all integrations" {
+  answers=(
+    "Star wars" # name
+    "nothing"   # machine_name
+    "nothing"   # org
+    "nothing"   # org_machine_name
+    "nothing"   # module_prefix
+    "nothing"   # profile
+    "nothing"   # theme
+    "nothing"   # URL
+    "nothing"   # webroot
+    "nothing"   # provision_use_profile
+    "curl"      # database_download_source
+    "file"      # database_store_type
+    "nothing"   # override_existing_db
+    "nothing"   # deploy_type
+    "y"         # preserve_ftp
+    "y"         # preserve_acquia
+    "y"         # preserve_lagoon
+    "y"         # preserve_renovatebot
+    "nothing"   # preserve_doc_comments
+    "nothing"   # preserve_vortex_info
+  )
+  output=$(run_installer_interactive "${answers[@]}")
+  assert_output_contains "WELCOME TO VORTEX INTERACTIVE INSTALLER"
+  assert_git_repo
+
+  install_dependencies_stub
+
+  assert_files_present_common
+  assert_files_present_no_provision_use_profile
+  assert_files_present_deployment
+  assert_files_present_integration_acquia
+  assert_files_present_integration_lagoon
+  assert_files_present_integration_ftp
+  assert_files_present_integration_renovatebot
+
+  assert_file_contains ".lagoon.yml" "name: Download database"
+  assert_file_contains ".lagoon.yml" "export VORTEX_PROVISION_OVERRIDE_DB=0"
+  assert_file_not_contains ".lagoon.yml" "# Explicitly set DB overwrite flag to the value from .env file for deployments from the profile."
+}
+
+@test "Install: empty directory; deployment - code" {
+  answers=(
+    "Star wars" # name
+    "nothing"   # machine_name
+    "nothing"   # org
+    "nothing"   # org_machine_name
+    "nothing"   # module_prefix
+    "nothing"   # profile
+    "nothing"   # theme
+    "nothing"   # URL
+    "nothing"   # webroot
+    "nothing"   # provision_use_profile
+    "curl"      # database_download_source
+    "file"      # database_store_type
+    "nothing"   # override_existing_db
+    "artifact"  # deploy_type
+    "y"         # preserve_ftp
+    "y"         # preserve_acquia
+    "y"         # preserve_lagoon
+    "y"         # preserve_renovatebot
+    "nothing"   # preserve_doc_comments
+    "nothing"   # preserve_vortex_info
+  )
+  output=$(run_installer_interactive "${answers[@]}")
+  assert_output_contains "WELCOME TO VORTEX INTERACTIVE INSTALLER"
+  assert_git_repo
+
+  install_dependencies_stub
+
+  assert_files_present_common
+  assert_files_present_no_provision_use_profile
+  assert_files_present_deployment
+  assert_files_present_integration_acquia
+  assert_files_present_integration_lagoon
+  assert_files_present_integration_ftp
+  assert_files_present_integration_renovatebot
+
+  assert_file_contains ".lagoon.yml" "name: Download database"
+  assert_file_contains ".lagoon.yml" "export VORTEX_PROVISION_OVERRIDE_DB=0"
+  assert_file_not_contains ".lagoon.yml" "# Explicitly set DB overwrite flag to the value from .env file for deployments from the profile."
+}
+
+@test "Install: empty directory; provision_use_profile" {
+  answers=(
+    "Star wars" # name
+    "nothing"   # machine_name
+    "nothing"   # org
+    "nothing"   # org_machine_name
+    "nothing"   # module_prefix
+    "nothing"   # profile
+    "nothing"   # theme
+    "nothing"   # URL
+    "nothing"   # webroot
+    "y"         # provision_use_profile
+    "nothing"   # override_existing_db
+    "artifact"  # deploy_type
+    "n"         # preserve_ftp
+    "n"         # preserve_acquia
+    "n"         # preserve_lagoon
+    "n"         # preserve_renovatebot
+    "nothing"   # preserve_doc_comments
+    "nothing"   # preserve_vortex_info
+  )
+  output=$(run_installer_interactive "${answers[@]}")
+  assert_output_contains "WELCOME TO VORTEX INTERACTIVE INSTALLER"
+  assert_git_repo
+
+  install_dependencies_stub
+
+  assert_files_present_common
+  assert_files_present_provision_use_profile
+  assert_files_present_deployment
+  assert_files_present_no_integration_acquia
+  assert_files_present_no_integration_lagoon
+  assert_files_present_no_integration_ftp
+  assert_files_present_no_integration_renovatebot
+}
+
+@test "Install: empty directory; provision_use_profile; Lagoon" {
+  answers=(
+    "Star wars" # name
+    "nothing"   # machine_name
+    "nothing"   # org
+    "nothing"   # org_machine_name
+    "nothing"   # module_prefix
+    "nothing"   # profile
+    "nothing"   # theme
+    "nothing"   # URL
+    "nothing"   # webroot
+    "y"         # provision_use_profile
+    "nothing"   # override_existing_db
+    "artifact"  # deploy_type
+    "n"         # preserve_ftp
+    "n"         # preserve_acquia
+    "y"         # preserve_lagoon
+    "n"         # preserve_renovatebot
+    "nothing"   # preserve_doc_comments
+    "nothing"   # preserve_vortex_info
+  )
+  output=$(run_installer_interactive "${answers[@]}")
+  assert_output_contains "WELCOME TO VORTEX INTERACTIVE INSTALLER"
+  assert_git_repo
+
+  install_dependencies_stub
+
+  assert_files_present_common
+  assert_files_present_provision_use_profile
+  assert_files_present_deployment
+  assert_files_present_no_integration_acquia
+  assert_files_present_integration_lagoon
+  assert_files_present_no_integration_ftp
+  assert_files_present_no_integration_renovatebot
+
+  assert_file_not_contains ".lagoon.yml" "name: Download database"
+  assert_file_not_contains ".lagoon.yml" "export VORTEX_PROVISION_OVERRIDE_DB=0"
+  assert_file_contains ".lagoon.yml" "# Explicitly set DB overwrite flag to the value from .env file for deployments from the profile."
+}

.vortex/tests/bats/workflow.smoke.bats
@@ -1,0 +1,86 @@
+#!/usr/bin/env bats
+#
+# Workflows using different types of install source.
+#
+
+# shellcheck disable=SC2030,SC2031,SC2129
+
+load _helper.bash
+load _helper.workflow.bash
+
+@test "Idempotence" {
+  prepare_sut "Starting idempotence tests in build directory ${BUILD_DIR}"
+
+  step "Download DEMO database"
+  assert_ahoy_download_db
+
+  step "Build project"
+  assert_ahoy_build
+  assert_gitignore
+  assert_ahoy_test_bdd_fast
+
+  # Running build several times should result in the same project build results.
+  step "Re-build project"
+  assert_ahoy_build
+  # Skip committing of the files.
+  assert_gitignore 1
+  assert_ahoy_test_bdd_fast
+}
+
+# Make sure to run with `TEST_GITHUB_TOKEN=working_test_token bats...` or this test will fail.
+@test "GitHub token" {
+  prepare_sut "Starting GitHub token tests in build directory ${BUILD_DIR}"
+
+  step "Add private package"
+  rm composer.lock || true
+  composer config repositories.test-private-package vcs git@github.com:drevops/test-private-package.git
+  jq --indent 4 '.require += {"drevops/test-private-package": "^1"}' composer.json >composer.json.tmp && mv -f composer.json.tmp composer.json
+
+  export VORTEX_CONTAINER_REGISTRY_USER="${TEST_VORTEX_CONTAINER_REGISTRY_USER?Test Docker user is not set}"
+  export VORTEX_CONTAINER_REGISTRY_PASS="${TEST_VORTEX_CONTAINER_REGISTRY_PASS?Test Docker pass is not set}"
+
+  step "Build without a GITHUB_TOKEN token"
+  unset GITHUB_TOKEN
+  process_ahoyyml
+  run ahoy build
+  assert_failure
+
+  step "Build with a GITHUB_TOKEN token"
+  export GITHUB_TOKEN="${TEST_GITHUB_TOKEN}"
+  process_ahoyyml
+  run ahoy build
+  assert_success
+}
+
+@test "Docker compose, no Ahoy" {
+  prepare_sut "Starting Docker compose without Ahoy tests in build directory ${BUILD_DIR}"
+
+  step "Download DEMO database"
+  assert_ahoy_download_db
+
+  step "Build project"
+  ahoy reset
+
+  substep "Building stack"
+  docker compose up -d --build --force-recreate >&3
+
+  substep "Installing dependencies"
+  docker compose exec -T cli composer install --prefer-dist >&3
+
+  substep "Provisioning"
+
+  # Copy DB into container for the cases when the volumes are not mounted.
+  # This will not be a case locally.
+  if [ "${VORTEX_DEV_VOLUMES_MOUNTED}" != "1" ]; then
+    if [ -f .data/db.sql ]; then
+      docker compose exec cli mkdir -p .data
+      docker compose cp -L .data/db.sql cli:/app/.data/db.sql
+    fi
+  fi
+
+  docker compose exec -T cli ./scripts/vortex/provision.sh >&3
+
+  sync_to_host
+  assert_gitignore
+  assert_ahoy_test_bdd_fast
+}

.vortex/tests/test.deployment.sh
@@ -1,0 +1,41 @@
+#!/usr/bin/env bash
+##
+# Run Vortex deployment tests.
+#
+# LCOV_EXCL_START
+
+set -eu
+[ "${VORTEX_DEBUG-}" = "1" ] && set -x
+
+ROOT_DIR="$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)")"
+
+SCRIPTS_DIR="${ROOT_DIR}/scripts/vortex"
+
+TEST_DIR="${ROOT_DIR}/.vortex/tests"
+
+# ------------------------------------------------------------------------------
+
+# Configure git username and email if it is not set.
+[ "$(git config --global user.name)" = "" ] && echo "==> Configuring global git user name" && git config --global user.name "Test user"
+[ "$(git config --global user.email)" = "" ] && echo "==> Configuring global git user email" && git config --global user.email "someone@example.com"
+
+# Create stub of local framework.
+docker network create amazeeio-network 2>/dev/null || true
+
+index="${TEST_NODE_INDEX:-*}"
+echo "==> Run deployment functional tests (${index})."
+[ ! -d "${TEST_DIR}/node_modules" ] && echo "  > Install test Node dependencies." && npm --prefix="${TEST_DIR}" ci
+
+bats() {
+  pushd "${ROOT_DIR}" >/dev/null || exit 1
+  if [ -n "${VORTEX_DEV_TEST_COVERAGE_DIR:-}" ]; then
+    mkdir -p "${VORTEX_DEV_TEST_COVERAGE_DIR}"
+    kcov --include-pattern=.sh,.bash --bash-parse-files-in-dir="${SCRIPTS_DIR}","${TEST_DIR}" --exclude-pattern=vendor,node_modules "${VORTEX_DEV_TEST_COVERAGE_DIR}" "${TEST_DIR}/node_modules/.bin/bats" "$@"
+  else
+    "${TEST_DIR}/node_modules/.bin/bats" "$@"
+  fi
+  popd >/dev/null || exit 1
+}
+
+# shellcheck disable=SC2086
+bats "${TEST_DIR}"/bats/deployment${index}.bats