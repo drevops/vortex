3171adf383b8e04b816d7f191baf483b4e5d1b96
Alex Skrypnyk <alex@drevops.com>
2024-10-07 16:28:43 +1100

    Added reporting of non-fixable rule to TwigCS Fixer.

--- .twig-cs-fixer.php
+++ .twig-cs-fixer.php
@@ -12,6 +12,7 @@ $finder->in(__DIR__ . '/web/themes/custom');
 $config = new TwigCsFixer\Config\Config();
 $config->setRuleset($ruleset);
 $config->setFinder($finder);
+$config->allowNonFixableRules();
 $config->addTokenParser(new Drupal\Core\Template\TwigTransTokenParser());
 
 return $config;
