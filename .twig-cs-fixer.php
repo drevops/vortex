c77acd45c0636896b56b0a80ae24eec573113ec7
Alex Skrypnyk <alex@drevops.com>
2024-12-03 22:33:18 +1100

    [#1455] Updated TwigCSFixer rules
    Closes #1455

--- .twig-cs-fixer.php
+++ .twig-cs-fixer.php
@@ -1,9 +1,23 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 $ruleset = new TwigCsFixer\Ruleset\Ruleset();
-$ruleset->addStandard(new TwigCsFixer\Standard\Twig());
+$ruleset->addRule(new TwigCsFixer\Rules\Delimiter\BlockNameSpacingRule());
+$ruleset->addRule(new TwigCsFixer\Rules\Delimiter\DelimiterSpacingRule());
+$ruleset->addRule(new TwigCsFixer\Rules\Function\NamedArgumentSpacingRule());
+$ruleset->addRule(new TwigCsFixer\Rules\Operator\OperatorNameSpacingRule());
+$ruleset->addRule(new TwigCsFixer\Rules\Operator\OperatorSpacingRule());
+$ruleset->addRule(new TwigCsFixer\Rules\Punctuation\PunctuationSpacingRule());
+$ruleset->addRule(new TwigCsFixer\Rules\Punctuation\TrailingCommaMultiLineRule());
+$ruleset->addRule(new TwigCsFixer\Rules\Punctuation\TrailingCommaSingleLineRule());
+$ruleset->addRule(new TwigCsFixer\Rules\String\HashQuoteRule());
+$ruleset->addRule(new TwigCsFixer\Rules\String\SingleQuoteRule());
+$ruleset->addRule(new TwigCsFixer\Rules\Variable\VariableNameRule());
+$ruleset->addRule(new TwigCsFixer\Rules\Whitespace\BlankEOFRule());
+$ruleset->addRule(new TwigCsFixer\Rules\Whitespace\EmptyLinesRule());
+$ruleset->addRule(new TwigCsFixer\Rules\Whitespace\IndentRule(2));
+$ruleset->addRule(new TwigCsFixer\Rules\Whitespace\TrailingSpaceRule());
 
 $finder = new TwigCsFixer\File\Finder();
 $finder->in(__DIR__ . '/web/modules/custom');
