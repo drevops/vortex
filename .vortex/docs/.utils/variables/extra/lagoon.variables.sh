8ba07ef6b1ae72c37429411cf7393354222382aa
Alex Skrypnyk <alex@drevops.com>
2025-01-31 19:03:28 +1100

    Added support for `DATABASE_DATABASE` DB name in settings.

--- .vortex/docs/.utils/variables/extra/lagoon.variables.sh
+++ .vortex/docs/.utils/variables/extra/lagoon.variables.sh
@@ -3,6 +3,9 @@
 # Additional environment variables used in this project in the Lagoon environment.
 # shellcheck disable=SC2034
 
+# Database name.
+DATABASE_DATABASE=
+
 # Enable New Relic in Lagoon environment.
 #
 # Set as project-wide variable.
