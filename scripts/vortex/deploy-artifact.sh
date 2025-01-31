8d49dc56a6f024c23bd06576fc2ddf84c22ac979
Alex Skrypnyk <alex@drevops.com>
2025-01-31 14:13:07 +1100

    Updated drevops/git-artifact to 0.8.

--- scripts/vortex/deploy-artifact.sh
+++ scripts/vortex/deploy-artifact.sh
@@ -78,7 +78,7 @@ info "Started ARTIFACT deployment."
 export VORTEX_SSH_PREFIX="DEPLOY" && . ./scripts/vortex/setup-ssh.sh
 
 note "Installing artifact builder."
-composer global require --dev -n --ansi --prefer-source --ignore-platform-reqs drevops/git-artifact:^0.7
+composer global require --dev -n --ansi --prefer-source --ignore-platform-reqs drevops/git-artifact:^0.8
 
 # Try resolving absolute paths.
 if command -v realpath >/dev/null 2>&1; then
