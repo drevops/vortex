9ac4381e63bbee0941d20c29f3a30bf3538c3768
Alex Skrypnyk <alex@drevops.com>
2025-03-06 16:39:38 +1100

    theme

--- .vortex/installer/src/Prompts/Handlers/Theme.php
+++ .vortex/installer/src/Prompts/Handlers/Theme.php
@@ -8,11 +8,11 @@ use DrevOps\Installer\Utils\File;
 
 class Theme extends AbstractHandler {
 
+  /**
+   * {@inheritdoc}
+   */
   public function discover(): null|string|bool|array {
-    $name_from_env = NULL;
-    if ($this->isInstalled()) {
-      $name_from_env = Env::getFromDotenv('DRUPAL_THEME', $this->dstDir);
-    }
+    $name_from_env = $this->isInstalled() ? Env::getFromDotenv('DRUPAL_THEME', $this->dstDir) : NULL;
 
     $file = static::findThemeFile($this->dstDir, $this->webroot);
 
@@ -25,9 +25,7 @@ class Theme extends AbstractHandler {
     $name_from_info = str_replace(['.info.yml', '.info'], '', basename($file));
 
     // Check that this is a theme coming originally from the Vortex template.
-    $dir = dirname($file);
-
-    if (!static::isVortexTheme($dir)) {
+    if (!static::isVortexTheme(dirname($file))) {
       // If the theme is not coming from the Vortex template - return the theme
       // name from the .env file.
       return $name_from_env ?: NULL;
@@ -47,18 +45,26 @@ class Theme extends AbstractHandler {
     return NULL;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function process(): void {
-    $webroot = $this->responses[WebrootCustom::id()];
     File::fileReplaceContent('/DRUPAL_THEME=.*/', 'DRUPAL_THEME=' . $this->response, $this->dstDir . '/.env');
 
     // Find the theme file in the destination directory.
-    $file_dst = static::findThemeFile($this->dstDir, $webroot);
+    $file_dst = static::findThemeFile($this->dstDir, $this->webroot);
+    $file_tmpl = static::findThemeFile($this->tmpDir, $this->webroot);
+
     // Remove the theme files from the template if not found OR if found, but
     // the theme is not from Vortex.
     if (empty($file_dst) || (!empty($file_dst) && !static::isVortexTheme(dirname($file_dst)))) {
-      $file_tmpl = static::findThemeFile($this->tmpDir, $webroot);
       if (!empty($file_tmpl) && is_readable($file_tmpl)) {
-         File::rmdirRecursive(dirname($file_tmpl));
+        File::rmdirRecursive(dirname($file_tmpl));
+      }
+    }
+    else {
+      if (!empty($file_tmpl)) {
+        File::replaceStringFilename('your_site_theme', $this->response, dirname($file_tmpl));
       }
     }
   }
