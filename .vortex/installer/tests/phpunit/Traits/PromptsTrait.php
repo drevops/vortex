9a0b10d5aebe899c77e00d19019589cf6633f859
Alex Skrypnyk <alex@drevops.com>
2025-03-08 14:09:41 +1100

    a

--- .vortex/installer/tests/phpunit/Traits/PromptsTrait.php
+++ .vortex/installer/tests/phpunit/Traits/PromptsTrait.php
@@ -10,6 +10,9 @@ use Laravel\Prompts\Prompt;
 trait PromptsTrait {
 
   protected static function promptsSetUp(): void {
+    // Override how validation is handled (it expects a user input on incorrect
+    // validation) to throw an exception instead so that we can assert on it
+    // in the tests.
     Prompt::validateUsing(function (Prompt $prompt) {
       if (is_callable($prompt->validate)) {
         $error = ($prompt->validate)($prompt->value());
@@ -30,15 +33,18 @@ trait PromptsTrait {
     $inputs = [];
 
     foreach ($responses as $response) {
-      // Null response means to use the default value.
+      // NULL response means to use the default value.
       if (!is_null($response)) {
         // Clear the input field default value.
         $inputs = array_merge($inputs, array_fill(0, 256, Key::BACKSPACE));
+        // Enter the response, one character at a time.
         $inputs = array_merge($inputs, mb_str_split($response));
       }
+      // Enter the response.
       $inputs[] = Key::ENTER;
     }
 
+    // Pass inputs to the prompt's fake method.
     Prompt::fake($inputs);
   }
 
