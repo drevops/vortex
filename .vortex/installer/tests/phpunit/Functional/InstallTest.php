4da304dc7921c25ea603d004d98113247675b2e7
Alex Skrypnyk <alex@drevops.com>
2025-03-10 18:55:41 +1100

    a

--- .vortex/installer/tests/phpunit/Functional/InstallTest.php
+++ .vortex/installer/tests/phpunit/Functional/InstallTest.php
@@ -25,6 +25,7 @@ class InstallTest extends FunctionalTestBase {
     //    // Custom welcome message.
     $this->assertTesterSuccessOutputContains('Welcome to Vortex interactive installer');
 
+    $this->assertFixtureDirectoryEqualsSut('post_install');
     //    $this->assertFixtureDirectoryEqualsSut('post_install');
     //    $this->assertComposerLockUpToDate();
   }
