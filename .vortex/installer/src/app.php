7693e24c9f21c087bb0f523d4276c63677e0cf28
Alex Skrypnyk <alex@drevops.com>
2025-03-10 16:00:32 +1100

    a

--- .vortex/installer/src/app.php
+++ .vortex/installer/src/app.php
@@ -10,9 +10,6 @@ declare(strict_types=1);
 use DrevOps\Installer\Command\InstallCommand;
 use Symfony\Component\Console\Application;
 
-// @todo Replace '@git-tag-ci@' with '@git-tag@' once the installer is moved-out
-// from the sub-directory as Box supports tag discovery from the root
-// directory that contains .git.
 $application = new Application('Vortex CLI installer', '@git-tag-ci@');
 
 $command = new InstallCommand();
