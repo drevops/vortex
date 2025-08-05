d8d349f7ac16018405ca5fedff3b596e628f95b5
Alex Skrypnyk <alex@drevops.com>
2025-08-05 16:32:44 +1000

    Updated format in `.ahoy.yml`.

--- .vortex/installer/src/Prompts/Handlers/Services.php
+++ .vortex/installer/src/Prompts/Handlers/Services.php
@@ -126,6 +126,8 @@ class Services extends AbstractHandler {
       @unlink($t . DIRECTORY_SEPARATOR . 'tests/behat/features/search.feature');
       File::replaceContentInFile($t . DIRECTORY_SEPARATOR . 'composer.json', '/\s*"drupal\/solr":\s*"[^\"]+",?\n/', "\n");
       File::replaceContentInFile($t . DIRECTORY_SEPARATOR . 'composer.json', '/\s*"drupal\/search_api_solr":\s*"[^\"]+",?\n/', "\n");
+      File::removeLine($t . DIRECTORY_SEPARATOR . '.ahoy.yml', 'VORTEX_HOST_SOLR_PORT=$(docker compose port solr 8983 2>/dev/null | cut -d : -f 2) && \\');
+      // @todo Remove after 25.10.0 release.
       File::removeLine($t . DIRECTORY_SEPARATOR . '.ahoy.yml', 'VORTEX_HOST_SOLR_PORT=$(docker compose port solr 8983 2>/dev/null | cut -d : -f 2) \\');
 
       $locations = [
