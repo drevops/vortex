c7b6bcbf3eed43ad848a2afdedde454ae539ce5f
Alex Skrypnyk <alex@drevops.com>
2025-09-02 12:09:18 +1000

    Fixed Tools discovery in installer.

--- .vortex/installer/src/Prompts/Handlers/Starter.php
+++ .vortex/installer/src/Prompts/Handlers/Starter.php
@@ -141,7 +141,7 @@ DOC;
 
     // @todo Update to use separate steps for hosting and CI/CD configuration.
     $output .= 'Setup integration with your hosting and CI/CD providers:' . PHP_EOL;
-    $output .= '  See https://www.vortextemplate.com/docs/quickstart';
+    $output .= '  See https://www.vortextemplate.com/docs/getting-started/installation';
     $output .= PHP_EOL;
 
     return $output;
