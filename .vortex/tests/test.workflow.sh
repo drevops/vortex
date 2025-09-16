650193e6064832c4a0dc0a52adaf8c492dc460de
Alex Skrypnyk <alex@drevops.com>
2025-09-17 05:58:22 +1000

    Removed repeating BATS test.

--- .vortex/tests/test.workflow.sh
+++ .vortex/tests/test.workflow.sh
@@ -74,7 +74,6 @@ case ${index} in
 
   4)
     bats "${TEST_DIR}"/bats/e2e/workflow.docker-compose.bats
-    bats "${TEST_DIR}"/bats/e2e/workflow.install.provision.bats
     bats "${TEST_DIR}"/bats/e2e/workflow.storage.image.bats
     # Disabled due to intermittent failures.
     # bats "${TEST_DIR}"/bats/e2e/workflow.storage.image_cached.bats
@@ -85,7 +84,6 @@ case ${index} in
     bats "${TEST_DIR}"/bats/e2e/workflow.install.profile.bats
     bats "${TEST_DIR}"/bats/e2e/workflow.install.profile_drupalcms.bats
     bats "${TEST_DIR}"/bats/e2e/workflow.docker-compose.bats
-    bats "${TEST_DIR}"/bats/e2e/workflow.install.provision.bats
     bats "${TEST_DIR}"/bats/e2e/workflow.storage.image.bats
     # Disabled due to intermittent failures.
     # bats "${TEST_DIR}"/bats/e2e/workflow.storage.image_cached.bats
