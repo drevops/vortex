2eec47fb6fcf5ead6847fecd6e67694f58795d18
Alex Skrypnyk <alex@drevops.com>
2026-02-09 21:24:17 +1100

    [#2261] Fixed Acquia settings to allow `DRUPAL_CONFIG_PATH` override independently of `config_vcs_directory`.

--- .vortex/installer/tests/Fixtures/handler_process/hosting_acquia/tests/phpunit/Drupal/EnvironmentSettingsTest.php
+++ .vortex/installer/tests/Fixtures/handler_process/hosting_acquia/tests/phpunit/Drupal/EnvironmentSettingsTest.php
@@ -49,7 +49,7 @@
      ];
    }
  
-@@ -315,6 +359,177 @@
+@@ -315,6 +359,221 @@
      $settings['skip_permissions_hardening'] = TRUE;
      $settings['config_sync_directory'] = '../config/default';
      $settings['suspend_mail_send'] = TRUE;
@@ -222,6 +222,50 @@
 +      'bower_components',
 +    ];
 +    $settings['config_sync_directory'] = '../config/default';
++    $settings['hash_salt'] = hash('sha256', getenv('DATABASE_HOST') ?: 'localhost');
++    $settings['maintenance_theme'] = 'claro';
++    $settings['trusted_host_patterns'] = [
++      '^localhost$',
++    ];
++    $this->assertSettings($settings);
++  }
++
++  /**
++   * Test Acquia config_sync_directory override with DRUPAL_CONFIG_PATH.
++   */
++  public function testEnvironmentAcquiaConfigPathOverride(): void {
++    $this->setEnvVars([
++      'AH_SITE_ENVIRONMENT' => 1,
++      'DRUPAL_CONFIG_PATH' => 'custom_acquia_config',
++    ]);
++
++    $this->requireSettingsFile();
++
++    $config['acquia_hosting_settings_autoconnect'] = FALSE;
++    $config['config_split.config_split.dev']['status'] = TRUE;
++    $config['environment_indicator.indicator']['bg_color'] = '#4caf50';
++    $config['environment_indicator.indicator']['fg_color'] = '#000000';
++    $config['environment_indicator.indicator']['name'] = self::ENVIRONMENT_DEV;
++    $config['environment_indicator.settings']['favicon'] = TRUE;
++    $config['environment_indicator.settings']['toolbar_integration'] = [TRUE];
++    $config['robotstxt.settings']['content'] = "User-agent: *\nDisallow: /";
++    $config['shield.settings']['shield_enable'] = TRUE;
++    $config['system.performance']['cache']['page']['max_age'] = 900;
++    $this->assertConfig($config);
++
++    $settings['auto_create_htaccess'] = TRUE;
++    $settings['config_exclude_modules'] = [];
++    $settings['config_sync_directory'] = 'custom_acquia_config';
++    $settings['container_yamls'][0] = $this->app_root . '/' . $this->site_path . '/services.yml';
++    $settings['entity_update_batch_size'] = 50;
++    $settings['environment'] = self::ENVIRONMENT_DEV;
++    $settings['file_public_path'] = 'sites/default/files';
++    $settings['file_private_path'] = 'sites/default/files/private';
++    $settings['file_temp_path'] = '/tmp';
++    $settings['file_scan_ignore_directories'] = [
++      'node_modules',
++      'bower_components',
++    ];
 +    $settings['hash_salt'] = hash('sha256', getenv('DATABASE_HOST') ?: 'localhost');
 +    $settings['maintenance_theme'] = 'claro';
      $settings['trusted_host_patterns'] = [
