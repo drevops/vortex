9c659c5fb22583692be7dec629ddf8a59438d558
Alex Skrypnyk <alex@drevops.com>
2026-02-19 17:18:12 +1100

    Extracted `FileManager` into `src/Utils/FileManager.php`.

--- .vortex/installer/src/Command/InstallCommand.php
+++ .vortex/installer/src/Command/InstallCommand.php
@@ -21,7 +21,7 @@ use DrevOps\VortexInstaller\Schema\SchemaGenerator;
 use DrevOps\VortexInstaller\Task\Task;
 use DrevOps\VortexInstaller\Utils\Config;
 use DrevOps\VortexInstaller\Utils\Env;
-use DrevOps\VortexInstaller\Utils\File;
+use DrevOps\VortexInstaller\Utils\FileManager;
 use DrevOps\VortexInstaller\Utils\OptionsResolver;
 use DrevOps\VortexInstaller\Utils\Tui;
 use Symfony\Component\Console\Command\Command;
@@ -84,6 +84,11 @@ class InstallCommand extends Command implements CommandRunnerAwareInterface, Exe
    * The installer presenter.
    */
   protected InstallerPresenter $presenter;
+
+  /**
+   * The file manager.
+   */
+  protected FileManager $fileManager;
 
   /**
    * The repository downloader.
@@ -173,6 +178,7 @@ EOF
       $this->promptManager = new PromptManager($this->config);
       $this->presenter = new InstallerPresenter($this->config);
       $this->presenter->setPromptManager($this->promptManager);
+      $this->fileManager = new FileManager($this->config);
 
       $this->presenter->header($this->artifact, $this->getApplication()->getVersion());
 
@@ -224,19 +230,19 @@ EOF
 
       Task::action(
         label: 'Preparing destination directory',
-        action: fn(): array => $this->prepareDestination(),
+        action: fn(): array => $this->fileManager->prepareDestination(),
         success: 'Destination directory is ready',
       );
 
       Task::action(
         label: 'Copying files to the destination directory',
-        action: fn() => $this->copyFiles(),
+        action: fn() => $this->fileManager->copyFiles(),
         success: 'Files copied to destination directory',
       );
 
       Task::action(
         label: 'Preparing demo content',
-        action: fn(): string|array => $this->prepareDemo(),
+        action: fn(): string|array => $this->fileManager->prepareDemo($this->getFileDownloader()),
         success: 'Demo content prepared',
       );
     }
@@ -349,117 +355,6 @@ EOF
     $output->write(AgentHelp::render());
 
     return Command::SUCCESS;
-  }
-
-  protected function prepareDestination(): array {
-    $messages = [];
-
-    $dst = $this->config->getDst();
-    if (!is_dir($dst)) {
-      $dst = File::mkdir($dst);
-      $messages[] = sprintf('Created directory "%s".', $dst);
-    }
-
-    if (!is_readable($dst . '/.git')) {
-      $messages[] = sprintf('Initialising a new Git repository in directory "%s".', $dst);
-      passthru(sprintf('git --work-tree="%s" --git-dir="%s/.git" init > /dev/null', $dst, $dst));
-
-      if (!File::exists($dst . '/.git')) {
-        throw new \RuntimeException(sprintf('Unable to initialise Git repository in directory "%s".', $dst));
-      }
-    }
-
-    return $messages;
-  }
-
-  protected function copyFiles(): void {
-    $src = $this->config->get(Config::TMP);
-    $dst = $this->config->getDst();
-
-    // Due to the way symlinks can be ordered, we cannot copy files one-by-one
-    // into destination directory. Instead, we are removing all ignored files
-    // and empty directories, making the src directory "clean", and then
-    // recursively copying the whole directory.
-    $all = File::scandir($src, File::ignoredPaths(), TRUE);
-    $files = File::scandir($src);
-    $valid_files = File::scandir($src, File::ignoredPaths());
-    $dirs = array_diff($all, $valid_files);
-    $ignored_files = array_diff($files, $valid_files);
-
-    foreach ($valid_files as $valid_file) {
-      $relative_file = str_replace($src . DIRECTORY_SEPARATOR, '.' . DIRECTORY_SEPARATOR, (string) $valid_file);
-
-      if (File::isInternal($relative_file)) {
-        unlink($valid_file);
-      }
-    }
-
-    // Remove skipped files.
-    foreach ($ignored_files as $ignored_file) {
-      if (is_readable($ignored_file)) {
-        unlink($ignored_file);
-      }
-    }
-
-    // Remove empty directories.
-    foreach ($dirs as $dir) {
-      File::rmdirIfEmpty($dir);
-    }
-
-    // Src directory is now "clean" - copy it to dst directory.
-    if (is_dir($src) && !File::dirIsEmpty($src)) {
-      File::copy($src, $dst);
-    }
-
-    // Special case for .env.local as it may exist.
-    if (!file_exists($dst . '/.env.local') && file_exists($dst . '/.env.local.example')) {
-      File::copy($dst . '/.env.local.example', $dst . '/.env.local');
-    }
-  }
-
-  /**
-   * Prepare demo content if in demo mode.
-   *
-   * @return array|string
-   *   Array of messages or a single message.
-   */
-  protected function prepareDemo(): array|string {
-    if (empty($this->config->get(Config::IS_DEMO))) {
-      return 'Not a demo mode.';
-    }
-
-    if (!empty($this->config->get(Config::IS_DEMO_DB_DOWNLOAD_SKIP))) {
-      return sprintf('%s is set. Skipping demo database download.', Config::IS_DEMO_DB_DOWNLOAD_SKIP);
-    }
-
-    // Reload variables from destination's .env.
-    Env::putFromDotenv($this->config->getDst() . '/.env');
-
-    $url = Env::get('VORTEX_DOWNLOAD_DB_URL');
-    if (empty($url)) {
-      return 'No database download URL provided. Skipping demo database download.';
-    }
-
-    $data_dir = $this->config->getDst() . DIRECTORY_SEPARATOR . Env::get('VORTEX_DB_DIR', './.data');
-    $db_file = Env::get('VORTEX_DB_FILE', 'db.sql');
-
-    if (file_exists($data_dir . DIRECTORY_SEPARATOR . $db_file)) {
-      return 'Database dump file already exists. Skipping demo database download.';
-    }
-
-    $messages = [];
-    if (!file_exists($data_dir)) {
-      $data_dir = File::mkdir($data_dir);
-      $messages[] = sprintf('Created data directory "%s".', $data_dir);
-    }
-
-    $destination = $data_dir . DIRECTORY_SEPARATOR . $db_file;
-    $this->getFileDownloader()->download($url, $destination);
-
-    $messages[] = sprintf('No database dump file was found in "%s" directory.', $data_dir);
-    $messages[] = sprintf('Downloaded demo database from %s.', $url);
-
-    return $messages;
   }
 
   /**
