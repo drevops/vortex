c71aee8912b9de27e3999e5996392a9b6e060b16
Alex Skrypnyk <alex@drevops.com>
2026-03-17 14:19:41 +1100

    Fixed CI failures from strict 'in_array()' and missing 'moderation_state' field.

--- web/sites/default/includes/modules/settings.clamav.php
+++ web/sites/default/includes/modules/settings.clamav.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
 
 if (file_exists($contrib_path . '/clamav') && !empty(getenv('DRUPAL_CLAMAV_ENABLED'))) {
   $clamav_mode = getenv('DRUPAL_CLAMAV_MODE') ?: NULL;
-  if (in_array(strtolower((string) $clamav_mode), ['0', 'daemon'])) {
+  if (in_array(strtolower((string) $clamav_mode), ['0', 'daemon'], true)) {
     // Drupal\clamav\Config::MODE_DAEMON.
     $config['clamav.settings']['scan_mode'] = 0;
     $config['clamav.settings']['mode_daemon_tcpip']['hostname'] = getenv('CLAMAV_HOST') ?: 'clamav';
