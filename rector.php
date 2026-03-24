695791c0f6fc6c7e7fd4be96a3beb41d96415a07
Alex Skrypnyk <alex@drevops.com>
2026-03-24 21:57:14 +1100

    [#2399] Added code fence markers around Behat attribute set in Rector configuration.

--- rector.php
+++ rector.php
@@ -82,8 +82,10 @@ return RectorConfig::configure()
   // PHP version upgrade sets - modernizes syntax to PHP 8.4.
   // Includes all rules from PHP 5.3 through 8.4.
   ->withPhpSets(php84: TRUE)
+  #;< TOOL_BEHAT
   // Behat attribute sets - converts annotations to PHP 8 attributes.
   ->withAttributesSets(behat: TRUE)
+  #;> TOOL_BEHAT
   // Code quality improvement sets.
   ->withPreparedSets(
     codeQuality: TRUE,
