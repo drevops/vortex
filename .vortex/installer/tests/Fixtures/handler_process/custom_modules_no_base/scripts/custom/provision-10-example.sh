cb8f80b9903ebc52b6fe93309f560dbf8dd32bc7
Alex Skrypnyk <alex@drevops.com>
2026-06-03 18:33:18 +1000

    Removed stale 'provision-10-example.sh' fixtures from legacy location.

--- .vortex/installer/tests/Fixtures/handler_process/custom_modules_no_base/scripts/custom/provision-10-example.sh
+++
@@ -1,8 +0,0 @@
-@@ -62,7 +62,6 @@
-   # Note that deployment hooks for already enabled modules have run in the
-   # parent "provision.sh" script.
-   task "Installing custom site modules."
--  drush pm:install sw_base
- 
-   drush pm:install sw_search
- 
--- .vortex/installer/tests/Fixtures/handler_process/custom_modules_no_search/scripts/custom/provision-10-example.sh
+++
@@ -1,9 +0,0 @@
-@@ -64,8 +64,6 @@
-   task "Installing custom site modules."
-   drush pm:install sw_base
- 
--  drush pm:install sw_search
--
-   drush pm:install sw_demo
-   pass "Installed custom site modules."
- 
