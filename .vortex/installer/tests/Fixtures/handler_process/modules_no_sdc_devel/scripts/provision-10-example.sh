5c7fb752f3654f4e8394d9f0bed06d1be463d22e
Alex Skrypnyk <alex@drevops.com>
2026-06-19 10:57:55 +1000

    Updated snapshots.

--- .vortex/installer/tests/Fixtures/handler_process/modules_no_sdc_devel/scripts/provision-10-example.sh
+++ .vortex/installer/tests/Fixtures/handler_process/modules_no_sdc_devel/scripts/provision-10-example.sh
@@ -1,9 +1,11 @@
-@@ -58,7 +58,7 @@
+@@ -57,10 +57,6 @@
+   drush pm:install search_api search_api_solr
    pass "Installed Solr search modules."
  
-   task "Installing Single Directory Component development tools."
+-  task "Installing Single Directory Component development tools."
 -  drush pm:install sdc_devel || true
-+  drush pm:install || true
-   pass "Installed Single Directory Component development tools."
- 
+-  pass "Installed Single Directory Component development tools."
+-
    # Enable custom site module and run its deployment hooks.
+   #
+   # Note that deployment hooks for already enabled modules have run in the
--- .vortex/installer/tests/Fixtures/handler_process/modules_none/scripts/provision-10-example.sh
+++ .vortex/installer/tests/Fixtures/handler_process/modules_none/scripts/provision-10-example.sh
@@ -9,12 +9,14 @@
    task "Installing Redis module."
    drush pm:install redis || true
    pass "Installed Redis module."
-@@ -58,7 +54,7 @@
+@@ -56,10 +52,6 @@
+   task "Installing Solr search modules."
+   drush pm:install search_api search_api_solr
    pass "Installed Solr search modules."
- 
-   task "Installing Single Directory Component development tools."
+-
+-  task "Installing Single Directory Component development tools."
 -  drush pm:install sdc_devel || true
-+  drush pm:install || true
-   pass "Installed Single Directory Component development tools."
+-  pass "Installed Single Directory Component development tools."
  
    # Enable custom site module and run its deployment hooks.
+   #
