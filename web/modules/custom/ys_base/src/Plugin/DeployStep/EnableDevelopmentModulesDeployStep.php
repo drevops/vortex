653abc1c68bdc260c2dc61e9a24085b2fc928d3b
Alex Skrypnyk <alex@drevops.com>
2026-06-20 15:58:17 +1000

    [#2651] Added SDC tooling, a sample button component, and CI validation. (#2671)
    Forward-ported from main 4cad2e0d4f40d2130b385c6611cad80d9f9511c1. Enabled 'sdc_devel' via the 'EnableDevelopmentModulesDeployStep' deploy step to match 2.x's provisioning model.

--- web/modules/custom/ys_base/src/Plugin/DeployStep/EnableDevelopmentModulesDeployStep.php
+++ web/modules/custom/ys_base/src/Plugin/DeployStep/EnableDevelopmentModulesDeployStep.php
@@ -91,6 +91,10 @@ final class EnableDevelopmentModulesDeployStep extends DeployStepBase {
     $this->moduleInstaller->install(['search_api', 'search_api_solr']);
     // phpcs:ignore #;> SERVICE_SEARCH
 
+    // phpcs:ignore #;< MODULE_SDC_DEVEL
+    $this->moduleInstaller->install(['sdc_devel']);
+    // phpcs:ignore #;> MODULE_SDC_DEVEL
+
     // phpcs:ignore #;< CUSTOM_MODULE_SEARCH
     $this->moduleInstaller->install(['ys_search']);
     // phpcs:ignore #;> CUSTOM_MODULE_SEARCH
