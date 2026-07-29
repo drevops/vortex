21cdc68b876fd913edc1dd80e9e30cd0975c3fd6
Alex Skrypnyk <alex@drevops.com>
2026-07-29 12:46:36 +1000

    Updated snapshots.

---
+++ .vortex/installer/tests/Fixtures/handler_process/_baseline/scripts/provision-10-enable-dev-modules.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+##
+# Enable development modules.
+#
+# This script is called during site provisioning via the provision script.
+#
+# Development modules are enabled here rather than in the example script so that
+# they remain available after the example script is adjusted or removed.
+
+set -eu
+[ "${VORTEX_DEBUG-}" = "1" ] && set -x
+
+# ------------------------------------------------------------------------------
+
+# @formatter:off
+info() { printf "   ==> %s\n" "${1}"; }
+note() { printf "       %s\n" "${1}"; }
+task() { printf "     > %s\n" "${1}"; }
+pass() { printf "     < %s\n" "${1}"; }
+fail() { printf "     ! %s\n" "${1}"; }
+# @formatter:on
+
+drush() { ./vendor/bin/drush -y "$@"; }
+
+# ------------------------------------------------------------------------------
+
+info "Started development modules operations."
+
+environment="$(drush php:eval "print \Drupal\core\Site\Settings::get('environment');")"
+note "Environment: ${environment}"
+
+if ! echo "${environment}" | grep -qxF -e local -e ci -e dev -e stage; then
+  note "Skipped installing development modules in production environment."
+  exit 0
+fi
+
+task "Installing Single Directory Component development tools."
+drush pm:install sdc_devel || true
+pass "Installed Single Directory Component development tools."
+
+task "Installing Devel module."
+drush pm:install devel || true
+pass "Installed Devel module."
+
+info "Finished development modules operations."
