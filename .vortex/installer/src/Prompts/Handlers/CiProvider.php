72c8947d6c86b7b04eaa4172f2ee5a9128f54bca
Alex Skrypnyk <alex@drevops.com>
2026-08-05 07:35:11 +1000

    [#2885] Moved security checks into a standalone 'Security audit' workflow.

--- .vortex/installer/src/Prompts/Handlers/CiProvider.php
+++ .vortex/installer/src/Prompts/Handlers/CiProvider.php
@@ -97,6 +97,7 @@ class CiProvider extends AbstractHandler {
 
     if ($remove_gha) {
       File::remove($t . '/.github/workflows/build-test-deploy.yml');
+      File::remove($t . '/.github/workflows/audit.yml');
       File::remove($t . '/' . $this->webroot . '/sites/default/includes/providers/settings.gha.php');
       File::removeTokenAsync('CI_PROVIDER_GHA');
       File::removeTokenAsync('SETTINGS_PROVIDER_GHA');
