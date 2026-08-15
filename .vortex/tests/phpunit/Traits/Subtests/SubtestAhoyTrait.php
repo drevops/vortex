3f1c1e5275a1550e40438a4cd1a0635c4532c425
Alex Skrypnyk <alex@drevops.com>
2026-08-16 09:47:12 +1000

    [#3023] Anchored environment variable prefixes forwarded by 'ahoy cli'.

--- .vortex/tests/phpunit/Traits/Subtests/SubtestAhoyTrait.php
+++ .vortex/tests/phpunit/Traits/Subtests/SubtestAhoyTrait.php
@@ -131,6 +131,40 @@ trait SubtestAhoyTrait {
       txt: '`ahoy cli` forwards a host ENVIRONMENT_TYPE into the container'
     );
 
+    // Prefixes are matched at the start of the name, so a host variable that
+    // merely contains one is not forwarded.
+    $this->cmdFail(
+      'ahoy cli \'printenv MY_DRUPAL_SECRET\'',
+      '! unforwardedvar',
+      env: ['MY_DRUPAL_SECRET' => 'unforwardedvar'],
+      txt: '`ahoy cli` does not forward a host variable that contains an allowed prefix mid-name'
+    );
+
+    // 'LOCALDEV_URL' is matched in full, so a name ending with it is not
+    // forwarded.
+    $this->cmdFail(
+      'ahoy cli \'printenv MY_LOCALDEV_URL\'',
+      '! unforwardedvar',
+      env: ['MY_LOCALDEV_URL' => 'unforwardedvar'],
+      txt: '`ahoy cli` does not forward a host variable that ends with an allowed name'
+    );
+
+    // 'TERM' is matched in full, so terminal variables built around it are not
+    // forwarded, while 'TERM' itself still is.
+    $this->cmdFail(
+      'ahoy cli \'printenv ITERM_PROFILE\'',
+      '! unforwardedvar',
+      env: ['ITERM_PROFILE' => 'unforwardedvar'],
+      txt: '`ahoy cli` does not forward a host variable that contains TERM'
+    );
+
+    $this->cmd(
+      'ahoy cli \'printenv TERM\'',
+      'anchoredtermvar',
+      env: ['TERM' => 'anchoredtermvar'],
+      txt: '`ahoy cli` forwards a host TERM into the container'
+    );
+
     $this->logStepFinish();
   }
 
