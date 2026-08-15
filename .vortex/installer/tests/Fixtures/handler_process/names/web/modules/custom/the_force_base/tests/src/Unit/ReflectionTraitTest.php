e1b7e61ad63d12d603817a431550be30f7b5acc9
Alex Skrypnyk <alex@drevops.com>
2026-08-16 08:50:53 +1000

    Updated snapshots.

---
+++ .vortex/installer/tests/Fixtures/handler_process/names/web/modules/custom/the_force_base/tests/src/Unit/ReflectionTraitTest.php
@@ -0,0 +1,138 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\the_force_base\Unit;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Group;
+
+/**
+ * Class ReflectionTraitTest.
+ *
+ * Tests for the ReflectionTrait.
+ *
+ * @package Drupal\the_force_base\Tests
+ */
+#[Group('TheForceBase')]
+class ReflectionTraitTest extends TheForceBaseUnitTestBase {
+
+  /**
+   * Tests reading a protected instance property.
+   */
+  #[DataProvider('dataProviderGetProtectedValue')]
+  public function testGetProtectedValue(mixed $value): void {
+    $object = new ReflectionTraitTestStub($value);
+
+    $this->assertSame($value, static::getProtectedValue($object, 'instanceValue'));
+  }
+
+  /**
+   * Data provider for testGetProtectedValue().
+   */
+  public static function dataProviderGetProtectedValue(): \Iterator {
+    yield ['instance value'];
+    yield [42];
+    yield [0.5];
+    yield [TRUE];
+    yield [NULL];
+    yield [['first', 'second']];
+    yield [new \stdClass()];
+  }
+
+  /**
+   * Tests that the value is read from the given instance.
+   */
+  public function testGetProtectedValueReadsGivenInstance(): void {
+    $first = new ReflectionTraitTestStub('first value');
+    $second = new ReflectionTraitTestStub('second value');
+
+    $this->assertSame('first value', static::getProtectedValue($first, 'instanceValue'));
+    $this->assertSame('second value', static::getProtectedValue($second, 'instanceValue'));
+  }
+
+  /**
+   * Tests reading a protected static property.
+   */
+  public function testGetProtectedValueStaticProperty(): void {
+    $object = new ReflectionTraitTestStub();
+
+    $this->assertSame('static value', static::getProtectedValue($object, 'staticValue'));
+  }
+
+  /**
+   * Tests reading a protected property declared on a parent class.
+   */
+  public function testGetProtectedValueInheritedProperty(): void {
+    $object = new ReflectionTraitTestChildStub('inherited value');
+
+    $this->assertSame('inherited value', static::getProtectedValue($object, 'instanceValue'));
+    $this->assertSame('child value', static::getProtectedValue($object, 'childValue'));
+  }
+
+  /**
+   * Tests reading a property that the object does not declare.
+   */
+  public function testGetProtectedValueMissingProperty(): void {
+    $object = new ReflectionTraitTestStub();
+
+    $this->expectException(\ReflectionException::class);
+    $this->expectExceptionMessage('does not exist');
+
+    static::getProtectedValue($object, 'missingValue');
+  }
+
+}
+
+/**
+ * Class ReflectionTraitTestStub.
+ *
+ * Stub with protected properties to read through reflection.
+ *
+ * @package Drupal\the_force_base\Tests
+ */
+class ReflectionTraitTestStub {
+
+  /**
+   * Protected static property.
+   *
+   * @var string
+   */
+  protected static string $staticValue = 'static value';
+
+  /**
+   * Protected instance property.
+   *
+   * @var mixed
+   */
+  protected mixed $instanceValue;
+
+  /**
+   * Constructs the stub.
+   *
+   * @param mixed $value
+   *   Value to assign to the protected instance property.
+   */
+  public function __construct(mixed $value = 'instance value') {
+    $this->instanceValue = $value;
+  }
+
+}
+
+/**
+ * Class ReflectionTraitTestChildStub.
+ *
+ * Stub inheriting the protected properties of its parent.
+ *
+ * @package Drupal\the_force_base\Tests
+ */
+class ReflectionTraitTestChildStub extends ReflectionTraitTestStub {
+
+  /**
+   * Protected property declared on the child.
+   *
+   * @var string
+   */
+  protected string $childValue = 'child value';
+
+}
