94c0e2b4dfc88fb570b0a1443f740ef22ce49cc4
Alex Skrypnyk <alex@drevops.com>
2026-08-17 20:46:25 +1000

    Updated snapshots.

--- .vortex/installer/tests/Fixtures/handler_process/tools_groups_no_be_tests_circleci/rector.php
+++ .vortex/installer/tests/Fixtures/handler_process/tools_groups_no_be_tests_circleci/rector.php
@@ -1,12 +1,12 @@
-@@ -37,7 +37,6 @@
+@@ -36,7 +36,6 @@
+ use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
  use Rector\Php80\Rector\Switch_\ChangeSwitchToMatchRector;
  use Rector\Php81\Rector\Array_\ArrayToFirstClassCallableRector;
- use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
 -use Rector\PHPUnit\CodeQuality\Rector\Class_\YieldDataProviderRector;
  use Rector\Privatization\Rector\ClassMethod\PrivatizeFinalClassMethodRector;
  use Rector\Privatization\Rector\MethodCall\PrivatizeLocalGetterToPropertyRector;
  use Rector\Privatization\Rector\Property\PrivatizeFinalClassPropertyRector;
-@@ -88,8 +87,6 @@
+@@ -84,8 +83,6 @@
    // PHP version upgrade sets - modernizes syntax to PHP 8.4.
    // Includes all rules from PHP 5.3 through 8.4.
    ->withPhpSets(php84: TRUE)
@@ -15,7 +15,7 @@
    // Code quality improvement sets.
    ->withPreparedSets(
      codeQuality: TRUE,
-@@ -109,7 +106,6 @@
+@@ -105,7 +102,6 @@
    // Additional rules.
    ->withRules([
      DeclareStrictTypesRector::class,
