70714d35e1d1516a2695c04f45b205cf57da7b77
Alex Skrypnyk <alex@drevops.com>
2026-08-19 14:18:47 +1000

    Updated template Composer dependencies and wired nine 'behat-steps' traits into 'FeatureContext'. (#3036)

--- .vortex/installer/tests/Fixtures/handler_process/modules_no_testmode/tests/behat/bootstrap/FeatureContext.php
+++ .vortex/installer/tests/Fixtures/handler_process/modules_no_testmode/tests/behat/bootstrap/FeatureContext.php
@@ -1,14 +1,14 @@
-@@ -26,7 +26,6 @@
- use DrevOps\BehatSteps\Drupal\RedirectTrait;
+@@ -33,7 +33,6 @@
  use DrevOps\BehatSteps\Drupal\SearchApiTrait;
+ use DrevOps\BehatSteps\Drupal\StateTrait;
  use DrevOps\BehatSteps\Drupal\TaxonomyTrait;
 -use DrevOps\BehatSteps\Drupal\TestmodeTrait;
  use DrevOps\BehatSteps\Drupal\UserTrait;
  use DrevOps\BehatSteps\Drupal\WatchdogTrait;
  use DrevOps\BehatSteps\ElementTrait;
-@@ -81,7 +80,6 @@
-   use RestTrait;
-   use SearchApiTrait;
+@@ -99,7 +98,6 @@
+   use StateTrait;
+   use TableTrait;
    use TaxonomyTrait;
 -  use TestmodeTrait;
    use UserTrait;
--- .vortex/installer/tests/Fixtures/handler_process/modules_none/tests/behat/bootstrap/FeatureContext.php
+++ .vortex/installer/tests/Fixtures/handler_process/modules_none/tests/behat/bootstrap/FeatureContext.php
@@ -1,23 +1,26 @@
-@@ -23,10 +23,8 @@
- use DrevOps\BehatSteps\MetatagTrait;
+@@ -29,11 +29,9 @@
  use DrevOps\BehatSteps\Drupal\OverrideTrait;
  use DrevOps\BehatSteps\Drupal\ParagraphsTrait;
+ use DrevOps\BehatSteps\Drupal\QueueTrait;
 -use DrevOps\BehatSteps\Drupal\RedirectTrait;
  use DrevOps\BehatSteps\Drupal\SearchApiTrait;
+ use DrevOps\BehatSteps\Drupal\StateTrait;
  use DrevOps\BehatSteps\Drupal\TaxonomyTrait;
 -use DrevOps\BehatSteps\Drupal\TestmodeTrait;
  use DrevOps\BehatSteps\Drupal\UserTrait;
  use DrevOps\BehatSteps\Drupal\WatchdogTrait;
  use DrevOps\BehatSteps\ElementTrait;
-@@ -75,13 +73,11 @@
-   use OverrideTrait;
+@@ -91,7 +89,6 @@
    use ParagraphsTrait;
    use PathTrait;
+   use QueueTrait;
 -  use RedirectTrait;
    use ResponseTrait;
    use ResponsiveTrait;
    use RestTrait;
-   use SearchApiTrait;
+@@ -99,7 +96,6 @@
+   use StateTrait;
+   use TableTrait;
    use TaxonomyTrait;
 -  use TestmodeTrait;
    use UserTrait;
