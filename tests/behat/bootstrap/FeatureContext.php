4f7bdcaff49a95f57cf9be0989e62e8f3e9231ff
Alex Skrypnyk <alex@drevops.com>
2026-08-31 13:16:53 +1000

    Updated template Composer dependencies and wired nine 'behat-steps' traits into 'FeatureContext'.
    (cherry picked from commit 70714d35e1d1516a2695c04f45b205cf57da7b77)
    Fixtures regenerated separately.

--- tests/behat/bootstrap/FeatureContext.php
+++ tests/behat/bootstrap/FeatureContext.php
@@ -8,10 +8,14 @@
 declare(strict_types=1);
 
 use DrevOps\BehatSteps\AccessibilityTrait;
+use DrevOps\BehatSteps\CommandTrait;
 use DrevOps\BehatSteps\CookieTrait;
 use DrevOps\BehatSteps\DateTrait;
+use DrevOps\BehatSteps\DiagnosticsTrait;
+use DrevOps\BehatSteps\Drupal\BigPipeTrait;
 use DrevOps\BehatSteps\Drupal\BlockTrait;
 use DrevOps\BehatSteps\Drupal\CacheTrait;
+use DrevOps\BehatSteps\Drupal\ConfigTrait;
 use DrevOps\BehatSteps\Drupal\ContentBlockTrait;
 use DrevOps\BehatSteps\Drupal\ContentTrait;
 use DrevOps\BehatSteps\Drupal\DraggableviewsTrait;
@@ -20,13 +24,16 @@ use DrevOps\BehatSteps\Drupal\EmailTrait;
 use DrevOps\BehatSteps\Drupal\FileTrait;
 use DrevOps\BehatSteps\Drupal\MediaTrait;
 use DrevOps\BehatSteps\Drupal\MenuTrait;
+use DrevOps\BehatSteps\Drupal\ModuleTrait;
 use DrevOps\BehatSteps\MetatagTrait;
 use DrevOps\BehatSteps\Drupal\OverrideTrait;
 use DrevOps\BehatSteps\Drupal\ParagraphsTrait;
+use DrevOps\BehatSteps\Drupal\QueueTrait;
 // phpcs:ignore #;< MODULE_REDIRECT
 use DrevOps\BehatSteps\Drupal\RedirectTrait;
 // phpcs:ignore #;> MODULE_REDIRECT
 use DrevOps\BehatSteps\Drupal\SearchApiTrait;
+use DrevOps\BehatSteps\Drupal\StateTrait;
 use DrevOps\BehatSteps\Drupal\TaxonomyTrait;
 use DrevOps\BehatSteps\Drupal\TestmodeTrait;
 use DrevOps\BehatSteps\Drupal\UserTrait;
@@ -39,10 +46,12 @@ use DrevOps\BehatSteps\JavascriptTrait;
 use DrevOps\BehatSteps\JsonTrait;
 use DrevOps\BehatSteps\KeyboardTrait;
 use DrevOps\BehatSteps\LinkTrait;
+use DrevOps\BehatSteps\ModalTrait;
 use DrevOps\BehatSteps\PathTrait;
 use DrevOps\BehatSteps\ResponseTrait;
 use DrevOps\BehatSteps\ResponsiveTrait;
 use DrevOps\BehatSteps\RestTrait;
+use DrevOps\BehatSteps\TableTrait;
 use DrevOps\BehatSteps\WaitTrait;
 use DrevOps\BehatSteps\XmlTrait;
 use Drupal\DrupalExtension\Context\DrupalContext;
@@ -53,12 +62,16 @@ use Drupal\DrupalExtension\Context\DrupalContext;
 class FeatureContext extends DrupalContext {
 
   use AccessibilityTrait;
+  use BigPipeTrait;
   use BlockTrait;
   use CacheTrait;
+  use CommandTrait;
+  use ConfigTrait;
   use ContentBlockTrait;
   use ContentTrait;
   use CookieTrait;
   use DateTrait;
+  use DiagnosticsTrait;
   use DraggableviewsTrait;
   use EckTrait;
   use ElementTrait;
@@ -74,9 +87,12 @@ class FeatureContext extends DrupalContext {
   use MediaTrait;
   use MenuTrait;
   use MetatagTrait;
+  use ModalTrait;
+  use ModuleTrait;
   use OverrideTrait;
   use ParagraphsTrait;
   use PathTrait;
+  use QueueTrait;
   // phpcs:ignore #;< MODULE_REDIRECT
   use RedirectTrait;
   // phpcs:ignore #;> MODULE_REDIRECT
@@ -84,6 +100,8 @@ class FeatureContext extends DrupalContext {
   use ResponsiveTrait;
   use RestTrait;
   use SearchApiTrait;
+  use StateTrait;
+  use TableTrait;
   use TaxonomyTrait;
   use TestmodeTrait;
   use UserTrait;
