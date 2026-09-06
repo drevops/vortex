df4fc3e8bfb07f76aed11e69faf160fdf6e275ff
Alex Skrypnyk <alex@drevops.com>
2026-09-06 14:49:44 +1000

    Updated snapshots.

--- .vortex/installer/tests/Fixtures/handler_process/tools_groups_no_fe_lint_no_theme_circleci/rector.php
+++ .vortex/installer/tests/Fixtures/handler_process/tools_groups_no_fe_lint_no_theme_circleci/rector.php
@@ -1,4 +1,4 @@
-@@ -44,7 +44,6 @@
+@@ -46,7 +46,6 @@
  return RectorConfig::configure()
    ->withPaths([
      __DIR__ . '/web/modules/custom',
@@ -6,7 +6,7 @@
      __DIR__ . '/web/sites/default/settings.php',
      __DIR__ . '/web/sites/default/includes',
      __DIR__ . '/tests',
-@@ -65,7 +64,6 @@
+@@ -69,7 +68,6 @@
      RemoveAlwaysTrueIfConditionRector::class,
      RemoveUnusedPublicMethodParameterRector::class => [
        __DIR__ . '/web/modules/custom/*/src/Hook/*',
