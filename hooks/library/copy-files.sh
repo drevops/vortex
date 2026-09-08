dbe21ae2354107952a319e19c3f85f9adb9b8189
Alex Skrypnyk <alex@drevops.com>
2026-09-09 09:00:36 +1000

    [#3111] Aborted Acquia hook wrappers on unset and empty required values.

--- hooks/library/copy-files.sh
+++ hooks/library/copy-files.sh
@@ -4,22 +4,22 @@
 #
 # Environment variables must be set in Acquia UI globally or for each environment.
 
-set -e
+set -eu
 [ "${VORTEX_DEBUG-}" = "1" ] && set -x
 
-site="${1}"
-target_env="${2}"
+site="${1:?Missing required site name.}"
+target_env="${2:?Missing required target environment name.}"
 
-pushd "/var/www/html/${site}.${target_env}" >/dev/null || exit 1
-
-[ "${VORTEX_TASK_COPY_FILES_ACQUIA_SKIP}" = "1" ] && echo "Skipping copying of files between Acquia environments." && exit 0
+[ "${VORTEX_TASK_COPY_FILES_ACQUIA_SKIP:-}" = "1" ] && echo "Skipping copying of files between Acquia environments." && exit 0
 
 export VORTEX_PLATFORM=acquia
-export VORTEX_ACQUIA_KEY="${VORTEX_ACQUIA_KEY?not set}"
-export VORTEX_ACQUIA_SECRET="${VORTEX_ACQUIA_SECRET?not set}"
+export VORTEX_ACQUIA_KEY="${VORTEX_ACQUIA_KEY:?Missing required value.}"
+export VORTEX_ACQUIA_SECRET="${VORTEX_ACQUIA_SECRET:?Missing required value.}"
 export VORTEX_ACQUIA_APP_NAME="${VORTEX_ACQUIA_APP_NAME:-${site}}"
 export VORTEX_TASK_COPY_FILES_ACQUIA_SRC="${VORTEX_TASK_COPY_FILES_ACQUIA_SRC:-prod}"
 export VORTEX_TASK_COPY_FILES_ACQUIA_DST="${VORTEX_TASK_COPY_FILES_ACQUIA_DST:-${target_env}}"
+
+pushd "/var/www/html/${site}.${target_env}" >/dev/null || exit 1
 
 ./vendor/bin/vortex-task copy-files
 
--- hooks/library/purge-cache.sh
+++ hooks/library/purge-cache.sh
@@ -4,22 +4,22 @@
 #
 # Environment variables must be set in Acquia UI globally or for each environment.
 
-set -e
+set -eu
 [ "${VORTEX_DEBUG-}" = "1" ] && set -x
 
-site="${1}"
-target_env="${2}"
+site="${1:?Missing required site name.}"
+target_env="${2:?Missing required target environment name.}"
 
-pushd "/var/www/html/${site}.${target_env}" >/dev/null || exit 1
-
-[ "${VORTEX_PURGE_CACHE_ACQUIA_SKIP}" = "1" ] && echo "Skipping purging of cache in Acquia environment." && exit 0
+[ "${VORTEX_PURGE_CACHE_ACQUIA_SKIP:-}" = "1" ] && echo "Skipping purging of cache in Acquia environment." && exit 0
 
 export VORTEX_PLATFORM=acquia
-export VORTEX_ACQUIA_KEY="${VORTEX_ACQUIA_KEY?not set}"
-export VORTEX_ACQUIA_SECRET="${VORTEX_ACQUIA_SECRET?not set}"
+export VORTEX_ACQUIA_KEY="${VORTEX_ACQUIA_KEY:?Missing required value.}"
+export VORTEX_ACQUIA_SECRET="${VORTEX_ACQUIA_SECRET:?Missing required value.}"
 export VORTEX_ACQUIA_APP_NAME="${VORTEX_ACQUIA_APP_NAME:-${site}}"
 export VORTEX_TASK_PURGE_CACHE_ACQUIA_ENV="${VORTEX_TASK_PURGE_CACHE_ACQUIA_ENV:-${target_env}}"
 export VORTEX_TASK_PURGE_CACHE_ACQUIA_DOMAINS_FILE="${VORTEX_TASK_PURGE_CACHE_ACQUIA_DOMAINS_FILE:-"/var/www/html/${site}.${target_env}/hooks/library/domains.txt"}"
+
+pushd "/var/www/html/${site}.${target_env}" >/dev/null || exit 1
 
 ./vendor/bin/vortex-task purge-cache
 
