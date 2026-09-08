bf25d13b4d4bc6e3cee46682d0312e799c7223b2
Alex Skrypnyk <alex@drevops.com>
2026-09-09 09:04:47 +1000

    Updated snapshots.

--- .vortex/installer/tests/Fixtures/handler_process/hosting_acquia/hooks/library/copy-db.sh
+++ .vortex/installer/tests/Fixtures/handler_process/hosting_acquia/hooks/library/copy-db.sh
@@ -4,23 +4,23 @@
 #
 # Environment variables must be set in Acquia UI globally or for each environment.
 
-set -e
+set -eu
 [ "${VORTEX_DEBUG-}" = "1" ] && set -x
 
-site="${1}"
-target_env="${2}"
+site="${1:?Missing required site name.}"
+target_env="${2:?Missing required target environment name.}"
 
-pushd "/var/www/html/${site}.${target_env}" >/dev/null || exit 1
-
-[ "${VORTEX_TASK_COPY_DB_ACQUIA_SKIP}" = "1" ] && echo "Skipping copying of database between Acquia environments." && exit 0
+[ "${VORTEX_TASK_COPY_DB_ACQUIA_SKIP:-}" = "1" ] && echo "Skipping copying of database between Acquia environments." && exit 0
 
 export VORTEX_PLATFORM=acquia
-export VORTEX_ACQUIA_KEY="${VORTEX_ACQUIA_KEY?not set}"
-export VORTEX_ACQUIA_SECRET="${VORTEX_ACQUIA_SECRET?not set}"
+export VORTEX_ACQUIA_KEY="${VORTEX_ACQUIA_KEY:?Missing required value.}"
+export VORTEX_ACQUIA_SECRET="${VORTEX_ACQUIA_SECRET:?Missing required value.}"
 export VORTEX_ACQUIA_APP_NAME="${VORTEX_ACQUIA_APP_NAME:-${site}}"
 export VORTEX_TASK_COPY_DB_ACQUIA_SRC="${VORTEX_TASK_COPY_DB_ACQUIA_SRC:-prod}"
 export VORTEX_TASK_COPY_DB_ACQUIA_DST="${VORTEX_TASK_COPY_DB_ACQUIA_DST:-${target_env}}"
-export VORTEX_TASK_COPY_DB_ACQUIA_NAME="${VORTEX_TASK_COPY_DB_ACQUIA_NAME?not set}"
+export VORTEX_TASK_COPY_DB_ACQUIA_NAME="${VORTEX_TASK_COPY_DB_ACQUIA_NAME:?Missing required value.}"
+
+pushd "/var/www/html/${site}.${target_env}" >/dev/null || exit 1
 
 ./vendor/bin/vortex-task copy-db
 
--- .vortex/installer/tests/Fixtures/handler_process/hosting_acquia/hooks/library/provision.sh
+++ .vortex/installer/tests/Fixtures/handler_process/hosting_acquia/hooks/library/provision.sh
@@ -3,11 +3,13 @@
 # Acquia Cloud hook: Provision site.
 #
 
-set -e
+set -eu
 [ "${VORTEX_DEBUG-}" = "1" ] && set -x
 
-site="${1}"
-target_env="${2}"
+site="${1:?Missing required site name.}"
+target_env="${2:?Missing required target environment name.}"
+
+[ "${VORTEX_PROVISION_ACQUIA_SKIP:-}" = "1" ] && echo "Skipping provisioning of the site in Acquia environment." && exit 0
 
 pushd "/var/www/html/${site}.${target_env}" >/dev/null || exit 1
 
