9d4a33345e43d3528a00fb38e0e7cffb3f74beaa
Alex Skrypnyk <alex@drevops.com>
2026-09-21 20:58:29 +1000

    Updated snapshots.

--- .vortex/installer/tests/Fixtures/handler_process/tools_groups_no_be_lint_circleci/tests/phpunit/Drupal/EnvironmentSettingsTest.php
+++ .vortex/installer/tests/Fixtures/handler_process/tools_groups_no_be_lint_circleci/tests/phpunit/Drupal/EnvironmentSettingsTest.php
@@ -9,47 +9,7 @@
   */
  #[Group('drupal_settings')]
  class EnvironmentSettingsTest extends SettingsTestCase {
-@@ -91,6 +87,7 @@
-       'generated_content',
-       'reroute_email',
-       'sdc_devel',
-+      'storybook',
-       'testmode',
-     ];
-     $settings['container_yamls'][0] = $this->app_root . '/' . $this->site_path . '/services.yml';
-@@ -188,6 +185,7 @@
-       'generated_content',
-       'reroute_email',
-       'sdc_devel',
-+      'storybook',
-       'testmode',
-     ];
-     $settings['config_sync_directory'] = 'custom_config';
-@@ -258,9 +256,11 @@
-       'generated_content',
-       'reroute_email',
-       'sdc_devel',
-+      'storybook',
-       'testmode',
-     ];
-     $settings['container_yamls'][0] = $this->app_root . '/' . $this->site_path . '/services.yml';
-+    $settings['container_yamls'][1] = $this->app_root . '/' . $this->site_path . '/includes/modules/services.storybook.yml';
-     $settings['entity_update_batch_size'] = 50;
-     $settings['environment'] = self::ENVIRONMENT_LOCAL;
-     $settings['fast404_allow_anon_imagecache'] = FALSE;
-@@ -329,9 +329,11 @@
-       'generated_content',
-       'reroute_email',
-       'sdc_devel',
-+      'storybook',
-       'testmode',
-     ];
-     $settings['container_yamls'][0] = $this->app_root . '/' . $this->site_path . '/services.yml';
-+    $settings['container_yamls'][1] = $this->app_root . '/' . $this->site_path . '/includes/modules/services.storybook.yml';
-     $settings['entity_update_batch_size'] = 50;
-     $settings['environment'] = self::ENVIRONMENT_LOCAL;
-     $settings['fast404_allow_anon_imagecache'] = FALSE;
-@@ -407,9 +409,9 @@
+@@ -407,9 +403,9 @@
    }
  
    /**
@@ -61,15 +21,3 @@
      $this->setEnvVars([
        'CI' => TRUE,
      ]);
-@@ -443,9 +445,11 @@
-       'generated_content',
-       'reroute_email',
-       'sdc_devel',
-+      'storybook',
-       'testmode',
-     ];
-     $settings['container_yamls'][0] = $this->app_root . '/' . $this->site_path . '/services.yml';
-+    $settings['container_yamls'][1] = $this->app_root . '/' . $this->site_path . '/includes/modules/services.storybook.yml';
-     $settings['entity_update_batch_size'] = 50;
-     $settings['environment'] = self::ENVIRONMENT_CI;
-     $settings['fast404_allow_anon_imagecache'] = FALSE;
